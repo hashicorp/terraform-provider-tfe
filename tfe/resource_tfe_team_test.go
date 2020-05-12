e4dccdf40c3d5415ca629f9660d8480d3e645d43
Ben Drucker <bvdrucker@gmail.com>
2020-05-12 10:18:08 -0700

    add test for updating team

--- tfe/resource_tfe_team_test.go
+++ tfe/resource_tfe_team_test.go
@@ -61,6 +61,54 @@ func TestAccTFETeam_full(t *testing.T) {
 	})
 }
 
+func TestAccTFETeam_full_update(t *testing.T) {
+	team := &tfe.Team{}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckTFETeamDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccTFETeam_full,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckTFETeamExists(
+						"tfe_team.foobar", team),
+					testAccCheckTFETeamAttributes_full(team),
+					resource.TestCheckResourceAttr(
+						"tfe_team.foobar", "name", "team-test"),
+					resource.TestCheckResourceAttr(
+						"tfe_team.foobar", "visibility", "organization"),
+					resource.TestCheckResourceAttr(
+						"tfe_team.foobar", "organization_access.0.manage_policies", "true"),
+					resource.TestCheckResourceAttr(
+						"tfe_team.foobar", "organization_access.0.manage_workspaces", "true"),
+					resource.TestCheckResourceAttr(
+						"tfe_team.foobar", "organization_access.0.manage_vcs_settings", "true"),
+				),
+			},
+			{
+				Config: testAccTFETeam_full_update,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckTFETeamExists(
+						"tfe_team.foobar", team),
+					testAccCheckTFETeamAttributes_full_update(team),
+					resource.TestCheckResourceAttr(
+						"tfe_team.foobar", "name", "team-test-1"),
+					resource.TestCheckResourceAttr(
+						"tfe_team.foobar", "visibility", "secret"),
+					resource.TestCheckResourceAttr(
+						"tfe_team.foobar", "organization_access.0.manage_policies", "false"),
+					resource.TestCheckResourceAttr(
+						"tfe_team.foobar", "organization_access.0.manage_workspaces", "false"),
+					resource.TestCheckResourceAttr(
+						"tfe_team.foobar", "organization_access.0.manage_vcs_settings", "false"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccTFETeam_import(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -145,6 +193,31 @@ func testAccCheckTFETeamAttributes_full(
 	}
 }
 
+func testAccCheckTFETeamAttributes_full_update(
+	team *tfe.Team) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if team.Name != "team-test-1" {
+			return fmt.Errorf("Bad name: %s", team.Name)
+		}
+
+		if team.Visibility != "secret" {
+			return fmt.Errorf("Bad visibility: %s", team.Visibility)
+		}
+
+		if team.OrganizationAccess.ManagePolicies {
+			return fmt.Errorf("OrganizationAccess.ManagePolicies should be false")
+		}
+		if team.OrganizationAccess.ManageVCSSettings {
+			return fmt.Errorf("OrganizationAccess.ManageVCSSettings should be false")
+		}
+		if team.OrganizationAccess.ManageWorkspaces {
+			return fmt.Errorf("OrganizationAccess.ManageWorkspaces should be false")
+		}
+
+		return nil
+	}
+}
+
 func testAccCheckTFETeamDestroy(s *terraform.State) error {
 	tfeClient := testAccProvider.Meta().(*tfe.Client)
 
@@ -193,5 +266,24 @@ resource "tfe_team" "foobar" {
     manage_policies = true
     manage_workspaces = true
     manage_vcs_settings = true
+  }
+}`
+
+const testAccTFETeam_full_update = `
+resource "tfe_organization" "foobar" {
+  name  = "tst-terraform"
+  email = "admin@company.com"
+}
+
+resource "tfe_team" "foobar" {
+  name         = "team-test-1"
+  organization = "${tfe_organization.foobar.id}"
+
+  visibility = "secret"
+  
+  organization_access {
+    manage_policies = false
+    manage_workspaces = false
+    manage_vcs_settings = false
   }
 }`
