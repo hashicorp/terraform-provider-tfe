228be2c70d14afbf737568cc5a6292f5aac8d596
Chris Arcand <chris@chrisarcand.com>
2020-05-25 08:45:34 -0500

    Fix untracked state on creation
    Because organization access is optional but returned by the API (for non
    Free plans), this should also be marked as computed - state is updated,
    so this avoids thrashing when the next plan wants to nullify these
    values.

--- tfe/resource_tfe_team.go
+++ tfe/resource_tfe_team.go
@@ -36,6 +36,7 @@ func resourceTFETeam() *schema.Resource {
 				Type:     schema.TypeList,
 				MaxItems: 1,
 				Optional: true,
+				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"manage_policies": {
