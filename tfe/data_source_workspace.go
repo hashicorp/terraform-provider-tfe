f84dd9875a5eca2a261383856d89610b3755ced5
Omar Ismail <oismail91@gmail.com>
2021-04-27 22:38:01 -0500

    r/tfe_workspace: Add remote state consumers

--- tfe/data_source_workspace.go
+++ tfe/data_source_workspace.go
@@ -44,6 +44,17 @@ func dataSourceTFEWorkspace() *schema.Resource {
 				Computed: true,
 			},
 
+			"global_remote_state": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+
+			"remote_state_consumer_ids": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+
 			"operations": {
 				Type:     schema.TypeBool,
 				Computed: true,
@@ -152,12 +163,25 @@ func dataSourceTFEWorkspaceRead(d *schema.ResourceData, meta interface{}) error 
 		}
 		return fmt.Errorf("Error retrieving workspace: %v", err)
 	}
+	remoteStateConsumers, err := tfeClient.Workspaces.RemoteStateConsumers(ctx, workspace.ID)
+	if err != nil {
+		if err == tfe.ErrResourceNotFound {
+			return fmt.Errorf("Could not find workspace %s/%s", organization, name)
+		}
+		return fmt.Errorf("Error retrieving remote state consumers for a workspace: %v", err)
+	}
+	consumerIDs := make([]string, len(remoteStateConsumers.Items))
+	for _, ws := range remoteStateConsumers.Items {
+		consumerIDs = append(consumerIDs, ws.ID)
+	}
 
 	// Update the config.
 	d.Set("allow_destroy_plan", workspace.AllowDestroyPlan)
 	d.Set("auto_apply", workspace.AutoApply)
 	d.Set("description", workspace.Description)
 	d.Set("file_triggers_enabled", workspace.FileTriggersEnabled)
+	d.Set("global_remote_state", workspace.GlobalRemoteState)
+	d.Set("remote_state_consumer_ids", consumerIDs)
 	d.Set("operations", workspace.Operations)
 	d.Set("queue_all_runs", workspace.QueueAllRuns)
 	d.Set("speculative_enabled", workspace.SpeculativeEnabled)
