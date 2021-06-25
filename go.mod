04e61a39e555f6f0dbfb3017e9faf43d24974a48
Omar Ismail <oismail91@gmail.com>
2021-06-25 14:46:48 -0400

    WIP

--- go.mod
+++ go.mod
@@ -3,13 +3,29 @@ module github.com/hashicorp/terraform-provider-tfe
 go 1.16
 
 require (
+	github.com/agext/levenshtein v1.2.3 // indirect
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/hashicorp/go-retryablehttp v0.6.8 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v0.16.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.0 // indirect
 	github.com/hashicorp/go-slug v0.7.0 // indirect
 	github.com/hashicorp/go-tfe v0.15.0
-	github.com/hashicorp/go-version v1.2.1
+	github.com/hashicorp/go-version v1.3.0
 	github.com/hashicorp/hcl v0.0.0-20180404174102-ef8a98b0bbce
+	github.com/hashicorp/terraform v1.0.0
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.6.0
-	github.com/hashicorp/terraform-svchost v0.0.0-20191011084731-65d371908596
+	github.com/hashicorp/terraform-svchost v0.0.0-20200729002733-f050f53b9734
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+	github.com/zclconf/go-cty v1.8.4
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/oauth2 v0.0.0-20210622215436-a8dc77f794b6 // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
 	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
 )
---
+++ tfe/data_source_remote_state.go
@@ -0,0 +1,79 @@
+package tfe
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	tfe "github.com/hashicorp/go-tfe"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceTFERemoteState() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceTFERemoteStateRead,
+
+		Schema: map[string]*schema.Schema{
+			"workspace": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"download_url": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"state_output": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+type stateFile struct {
+	Outputs map[string]outputValue `json:"outputs"`
+}
+
+type outputValue struct {
+	Type  string      `json:"type"`
+	Value interface{} `json:"value"`
+}
+
+func dataSourceTFERemoteStateRead(d *schema.ResourceData, meta interface{}) error {
+	tfeClient := meta.(*tfe.Client)
+
+	wsID := d.Get("workspace").(string)
+	log.Printf("[DEBUG] Read remote state for and Workspace %s", wsID)
+
+	sv, err := tfeClient.StateVersions.Current(ctx, wsID)
+	if err != nil {
+		if err == tfe.ErrResourceNotFound {
+			return fmt.Errorf("Could not read  remote state for workspace '%s'", wsID)
+		}
+		return fmt.Errorf("Error remote state: %v", err)
+	}
+
+	log.Printf("[DEBUG] Setting Remote State Output")
+	d.SetId(sv.ID)
+
+	d.Set("download_url", sv.DownloadURL)
+	stateData, err := tfeClient.StateVersions.Download(ctx, sv.DownloadURL)
+	if err != nil {
+		return fmt.Errorf("Error downloading remote state: %v", err)
+	}
+	stateOuptput := &stateFile{}
+	if err := json.Unmarshal(stateData, stateOuptput); err != nil {
+		return err
+	}
+	log.Printf("[DEBUG] STATE OUTPUT: %v", stateOuptput)
+
+	for k, v := range stateOuptput.Outputs {
+		log.Printf("[DEBUG] STATE KEY: %s", k)
+		log.Printf("[DEBUG] STATE VALUE: %s", v.Value)
+	}
+	d.Set("state_output", "foo")
+
+	return nil
+}
