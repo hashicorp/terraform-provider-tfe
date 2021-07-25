64610383e3cb7d66333d1b610b22e670939ffcb1
Omar Ismail <oismail91@gmail.com>
2021-07-25 15:49:55 -0400

    Got state output to work

--- tfe/data_source_remote_state.go
+++ tfe/data_source_remote_state.go
@@ -1,20 +1,25 @@
 package tfe
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"math/big"
+	//"math/big"
 	"os"
 
 	tfe "github.com/hashicorp/go-tfe"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5/tftypes"
+	"github.com/zclconf/go-cty/cty"
+	//gocty "github.com/zclconf/go-cty/cty/gocty"
+	ctyjson "github.com/zclconf/go-cty/cty/json"
 )
 
 type dataSourceRemoteState struct {
-	meta providerMeta
+	provider *providerServer
 }
 
 var stderr *os.File
@@ -23,93 +28,95 @@ func init() {
 	stderr = os.Stderr
 }
 
-func (d dataSourceRemoteState) ReadDataSource(ctx context.Context, req *tfprotov5.ReadDataSourceRequest) (*tfprotov5.ReadDataSourceResponse, error) {
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR")
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR ---------META")
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR ---------META")
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR ---------META: %v", d.meta)
-	hostname, token, err := retrieveMeta(req)
-	if err != nil {
-		return nil, err
+func newDataSourceRemoteState(p *providerServer) tfprotov5.DataSourceServer {
+	return dataSourceRemoteState{
+		provider: p,
 	}
+}
 
-	client, err := getClient(hostname, token, false)
+func (d dataSourceRemoteState) ReadDataSource(ctx context.Context, req *tfprotov5.ReadDataSourceRequest) (*tfprotov5.ReadDataSourceResponse, error) {
+	resp := &tfprotov5.ReadDataSourceResponse{
+		Diagnostics: []*tfprotov5.Diagnostic{},
+	}
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: (dataSourceRemoteState) ReadDataSource")
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: (dataSourceRemoteState) ReadDataSource")
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: (dataSourceRemoteState) ReadDataSource")
+	client, err := getClient(d.provider.meta.hostname, d.provider.meta.token, false)
 	if err != nil {
 		return nil, err
 	}
 
 	orgName, wsName, err := retrieveValues(req)
 	if err != nil {
-		return &tfprotov5.ReadDataSourceResponse{
-			Diagnostics: []*tfprotov5.Diagnostic{
-				{
-					Severity: tfprotov5.DiagnosticSeverityError,
-					Summary:  "Error retrieving values from the config",
-					Detail:   fmt.Sprintf("Error retrieving values from the config: %s", err.Error()),
-				},
-			},
-		}, nil
+		resp.Diagnostics = append(resp.Diagnostics, &tfprotov5.Diagnostic{
+			Severity: tfprotov5.DiagnosticSeverityError,
+			Summary:  "Error retrieving values from the config",
+			Detail:   fmt.Sprintf("Error retrieving values from the config: %v", err),
+		})
+		return resp, nil
 	}
 
-	_ = d.readRemoteStateOutput(ctx, client, orgName, wsName)
-
-	/*
-		stateFile := map[string]interface{}{
-			"foo":   []interface{}{"a", "b", "c"},
-			"hello": "world",
-		}
-	*/
-	stateFile := map[string]interface{}{
-		"foo":   []interface{}{"a", "b", "c"},
-		"hello": int64(123),
-		"quuz":  false,
+	remoteStateOutput, err := d.readRemoteStateOutput(ctx, client, orgName, wsName)
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: remoteStateOutput: ", remoteStateOutput)
+	if err != nil {
+		resp.Diagnostics = append(resp.Diagnostics, &tfprotov5.Diagnostic{
+			Severity: tfprotov5.DiagnosticSeverityError,
+			Summary:  "Error reading remote state output",
+			Detail:   fmt.Sprintf("Error reading remote state output: %v", err),
+		})
+		return resp, nil
 	}
+	//parsedStateOutput := parseRemoteStateOutput(remoteStateOutput)
+	//log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: parsedStateOutput: ", parsedStateOutput)
+
 	tftypesState := map[string]tftypes.Value{}
 	stateTypes := map[string]tftypes.Type{}
-	for k, v := range stateFile {
-		switch val := v.(type) {
-		case string:
-			tftypesState[k] = tftypes.NewValue(tftypes.String, val)
-			stateTypes[k] = tftypes.String
-		case int64:
-			tftypesState[k] = tftypes.NewValue(tftypes.Number, big.NewFloat(float64(val)))
-			stateTypes[k] = tftypes.Number
-		case bool:
-			tftypesState[k] = tftypes.NewValue(tftypes.Bool, val)
-			stateTypes[k] = tftypes.Bool
-		case []interface{}:
-			elements := []tftypes.Value{}
-			types := []tftypes.Type{}
-			for _, element := range val {
-				switch el := element.(type) {
-				case string:
-					elements = append(elements, tftypes.NewValue(tftypes.String, el))
-					types = append(types, tftypes.String)
-				default:
-					panic(fmt.Sprintf("unknown type %T", element))
-				}
-			}
-			tftypesState[k] = tftypes.NewValue(tftypes.Tuple{ElementTypes: types}, elements)
-			stateTypes[k] = tftypes.Tuple{ElementTypes: types}
+
+	for name, outputValues := range remoteStateOutput.OutputValues {
+		marshData, err := outputValues.Value.Type().MarshalJSON()
+		if err != nil {
+			log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: EEROOOOOOOOORRRRRRRR %v", err)
+			//return err
 		}
+		tfType, err := tftypes.ParseJSONType(marshData)
+		if err != nil {
+			log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: EEROOOOOOOOORRRRRRRR %v", err)
+			//return err
+		}
+		mByte, err := ctyjson.Marshal(outputValues.Value, outputValues.Value.Type())
+		if err != nil {
+			log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: EEROOOOOOOOORRRRRRRR %v", err)
+			//return err
+		}
+		rawState := tfprotov5.RawState{
+			JSON: mByte,
+		}
+		newVal, err := rawState.Unmarshal(tfType)
+		if err != nil {
+			log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: EEROOOOOOOOORRRRRRRR")
+			//	return fmt.Errorf("ERROOR %v", err)
+		}
+		//newVal := tftypes.NewValue(tfType, outV)
+		tftypesState[name] = newVal
+		stateTypes[name] = tfType
 	}
 
 	state, err := tfprotov5.NewDynamicValue(tftypes.Object{
 		AttributeTypes: map[string]tftypes.Type{
 			"workspace":    tftypes.String,
 			"organization": tftypes.String,
-			"state_output": tftypes.DynamicPseudoType,
+			"values":       tftypes.DynamicPseudoType,
 		},
 	}, tftypes.NewValue(tftypes.Object{
 		AttributeTypes: map[string]tftypes.Type{
 			"workspace":    tftypes.String,
 			"organization": tftypes.String,
-			"state_output": tftypes.Object{AttributeTypes: stateTypes},
+			"values":       tftypes.Object{AttributeTypes: stateTypes},
 		},
 	}, map[string]tftypes.Value{
 		"workspace":    tftypes.NewValue(tftypes.String, wsName),
 		"organization": tftypes.NewValue(tftypes.String, orgName),
-		"state_output": tftypes.NewValue(tftypes.Object{AttributeTypes: stateTypes}, tftypesState),
+		"values":       tftypes.NewValue(tftypes.Object{AttributeTypes: stateTypes}, tftypesState),
 	}))
 
 	if err != nil {
@@ -142,7 +149,7 @@ func retrieveValues(req *tfprotov5.ReadDataSourceRequest) (string, string, error
 		AttributeTypes: map[string]tftypes.Type{
 			"workspace":    tftypes.String,
 			"organization": tftypes.String,
-			"state_output": tftypes.String,
+			"values":       tftypes.String,
 		}})
 	if err != nil {
 		return orgName, wsName, fmt.Errorf("Error unmarshalling config: %v", err)
@@ -166,88 +173,214 @@ func retrieveValues(req *tfprotov5.ReadDataSourceRequest) (string, string, error
 	return orgName, wsName, nil
 }
 
-func retrieveMeta(req *tfprotov5.ReadDataSourceRequest) (string, string, error) {
-	var hostname string
-	var token string
-	config := req.ProviderMeta
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR META CONFIG %v", config)
-	val, err := config.Unmarshal(tftypes.Object{
-		AttributeTypes: map[string]tftypes.Type{
-			"hostname":        tftypes.String,
-			"token":           tftypes.String,
-			"ssl_skip_verify": tftypes.Bool,
-		}})
-	if err != nil {
-		return hostname, token, fmt.Errorf("Error unmarshalling config: %v", err)
-	}
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR META VAL %v", val)
-
-	var valMap map[string]tftypes.Value
-	err = val.As(&valMap)
-	if err != nil {
-		return hostname, token, fmt.Errorf("Error assigning Value to Golang map: %v", err)
-	}
-
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR META VAL %v", valMap)
-
-	err = valMap["hostname"].As(&hostname)
-	if err != nil {
-		return hostname, token, fmt.Errorf("Error assigning 'organization' Value to Golang string: %v", err)
-	}
-	err = valMap["token"].As(&token)
-	if err != nil {
-		return hostname, token, fmt.Errorf("Error assigning 'workspace' Value to Golang string: %v", err)
-	}
-
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR META VAL hostname %s", hostname)
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR META VAL token %s", token)
-
-	hostname = "app.terraform.io"
-	token = "foo"
-	return hostname, token, nil
-
-}
-
-type remoteStateFile struct {
-	Outputs map[string]interface{} `json:"outputs"`
+type remoteState struct {
+	Outputs map[string]outputValue `json:"outputs"`
 }
 
 type outputValue struct {
-	Type  string      `json:"type"`
+	Type  interface{} `json:"type"`
 	Value interface{} `json:"value"`
 }
 
-func (d dataSourceRemoteState) readRemoteStateOutput(ctx context.Context, tfeClient *tfe.Client, orgName, wsName string) error {
+type stateV4 struct {
+	RootOutputs map[string]outputStateV4 `json:"outputs"`
+}
 
+type outputStateV4 struct {
+	ValueRaw     json.RawMessage `json:"value"`
+	ValueTypeRaw json.RawMessage `json:"type"`
+	Sensitive    bool            `json:"sensitive,omitempty"`
+}
+
+type OutputValue struct {
+	Addr      AbsOutputValue
+	Value     cty.Value
+	Sensitive bool
+}
+
+type AbsOutputValue struct {
+	OutputValue OutputValueTwo
+}
+
+type OutputValueTwo struct {
+	Name string
+}
+
+type FinalOutputValue struct {
+	OutputValues map[string]*OutputValue
+}
+
+func (d dataSourceRemoteState) readRemoteStateOutput(ctx context.Context, tfeClient *tfe.Client, orgName, wsName string) (*FinalOutputValue, error) {
+	log.Printf("[DEBUG] Reading the Workspace %s in Organization %s", wsName, orgName)
 	ws, err := tfeClient.Workspaces.Read(ctx, orgName, wsName)
 	if err != nil {
-		return fmt.Errorf("Error reading workspace: %v", err)
+		return nil, fmt.Errorf("Error reading workspace: %v", err)
 	}
 
+	/*
+
+		TODO: CHeck this query
+		   st, err := cli.StateVersions.CurrentWithOptions(ctx, ws.ID, &tfe.StateVersionCurrentOptions{
+		              Include: "outputs",
+		      })
+	*/
+	log.Printf("[DEBUG] Reading the current StateVersion for Workspace ID %s.", ws.ID)
 	sv, err := tfeClient.StateVersions.Current(ctx, ws.ID)
 	if err != nil {
 		if err == tfe.ErrResourceNotFound {
-			return fmt.Errorf("Could not read  remote state for workspace '%s'", wsName)
+			return nil, fmt.Errorf("Could not read  remote state for workspace '%s'", wsName)
 		}
-		return fmt.Errorf("Error remote state: %v", err)
+		return nil, fmt.Errorf("Error remote state: %v", err)
 	}
 
-	log.Printf("[DEBUG] Setting Remote State Output")
-
+	log.Printf("[DEBUG] Downloading State Version")
 	stateData, err := tfeClient.StateVersions.Download(ctx, sv.DownloadURL)
 	if err != nil {
-		return fmt.Errorf("Error downloading remote state: %v", err)
-	}
-	stateOuptput := &remoteStateFile{}
-	if err := json.Unmarshal(stateData, stateOuptput); err != nil {
-		return err
-	}
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR STATE OUTPUT: %v", stateOuptput)
-
-	for k, v := range stateOuptput.Outputs {
-		log.Printf("[DEBUG] ============OMAR=========== STATE KEY: %s", k)
-		log.Printf("[DEBUG] ============OMAR=========== STATE VALUE: %s", v)
+		return nil, fmt.Errorf("Error downloading remote state: %v", err)
 	}
 
+	log.Printf("[DEBUG] Unmarshalling remote state output")
+	read := bytes.NewReader(stateData)
+	src, err := ioutil.ReadAll(read)
+	if err != nil {
+		return nil, fmt.Errorf("ERROOR %v", err)
+	}
+	sV4 := &stateV4{}
+	err = json.Unmarshal(src, sV4)
+	if err != nil {
+		return nil, fmt.Errorf("ERROOR %v", err)
+	}
+
+	fov := &FinalOutputValue{
+		OutputValues: map[string]*OutputValue{},
+	}
+	for name, fos := range sV4.RootOutputs {
+		os := &OutputValue{
+			Addr: AbsOutputValue{
+				OutputValue: OutputValueTwo{
+					Name: name,
+				},
+			},
+		}
+		os.Sensitive = fos.Sensitive
+
+		ty, err := ctyjson.UnmarshalType([]byte(fos.ValueTypeRaw))
+		if err != nil {
+			return nil, fmt.Errorf("ERROOR %v", err)
+		}
+
+		val, err := ctyjson.Unmarshal([]byte(fos.ValueRaw), ty)
+		if err != nil {
+			return nil, fmt.Errorf("ERROOR %v", err)
+		}
+
+		os.Value = val
+		fov.OutputValues[name] = os
+	}
+
+	//stateOuptput := &remoteState{}
+	//if err := json.Unmarshal(stateData, stateOuptput); err != nil {
+	//	return nil, fmt.Errorf("Could not unmarshal remote state output %v", err)
+	//}
+
+	return fov, nil
+}
+
+func parseRemoteStateOutput(stateOutput *FinalOutputValue) error {
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: parseRemoteStateOutput")
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: parseRemoteStateOutput")
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: parseRemoteStateOutput")
+
+	tftypesState := map[string]tftypes.Value{}
+	stateTypes := map[string]tftypes.Type{}
+
+	for name, outputValues := range stateOutput.OutputValues {
+		log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: NAME: ", name)
+		log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: VALUE: ", outputValues.Value)
+		log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: TYPE: ", outputValues.Value.Type())
+		log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: KEY: ", outputValues)
+
+		marshData, err := outputValues.Value.Type().MarshalJSON()
+		if err != nil {
+			log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: EEROOOOOOOOORRRRRRRR %v", err)
+			//return err
+		}
+		tfType, err := tftypes.ParseJSONType(marshData)
+		if err != nil {
+			//return err
+			log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: EEROOOOOOOOORRRRRRRR %v", err)
+		}
+		log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: TF TYPE: ", tfType)
+
+		mByte, err := ctyjson.Marshal(outputValues.Value, outputValues.Value.Type())
+		if err != nil {
+			//return err
+			log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: EEROOOOOOOOORRRRRRRR %v", err)
+		}
+		log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: BYTE: ", string(mByte))
+
+		outV := []string{}
+		//err = gocty.FromCtyValue(outputValues.Value, &outV)
+		//err = json.Unmarshal(mByte, &outV)
+		if err != nil {
+			log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: EEROOOOOOOOORRRRRRRR %v", err)
+			//return fmt.Errorf("ERROOR %v", err)
+		}
+		log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: OUTPUT VALUE: ", outV)
+
+		log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: VALUE INTERFCE: ", tfType)
+
+		rawState := tfprotov5.RawState{
+			JSON: mByte,
+		}
+		newVal, err := rawState.Unmarshal(tfType)
+		if err != nil {
+			log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: EEROOOOOOOOORRRRRRRR")
+			//return fmt.Errorf("ERROOR %v", err)
+		}
+		//newVal := tftypes.NewValue(tfType, outV)
+		log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR: NEW VAL: ", newVal)
+		tftypesState[name] = newVal
+		stateTypes[name] = tfType
+	}
 	return nil
 }
+
+/*
+	stateFile := map[string]interface{}{
+		"foo":   []interface{}{"a", "b", "c"},
+		"hello": "world",
+	}
+*/
+//	stateFile := map[string]interface{}{
+//		"foo":   []interface{}{"a", "b", "c"},
+//		"hello": int64(123),
+//		"quuz":  false,
+//	}
+//for k, v := range stateFile {
+//	switch val := v.(type) {
+//	case string:
+//		tftypesState[k] = tftypes.NewValue(tftypes.String, val)
+//		stateTypes[k] = tftypes.String
+//	case int64:
+//		tftypesState[k] = tftypes.NewValue(tftypes.Number, big.NewFloat(float64(val)))
+//		stateTypes[k] = tftypes.Number
+//	case bool:
+//		tftypesState[k] = tftypes.NewValue(tftypes.Bool, val)
+//		stateTypes[k] = tftypes.Bool
+//	case []interface{}:
+//		elements := []tftypes.Value{}
+//		types := []tftypes.Type{}
+//		for _, element := range val {
+//			switch el := element.(type) {
+//			case string:
+//				elements = append(elements, tftypes.NewValue(tftypes.String, el))
+//				types = append(types, tftypes.String)
+//			default:
+//				panic(fmt.Sprintf("unknown type %T", element))
+//			}
+//		}
+//		tftypesState[k] = tftypes.NewValue(tftypes.Tuple{ElementTypes: types}, elements)
+//		stateTypes[k] = tftypes.Tuple{ElementTypes: types}
+//	}
+//}
--- tfe/provider_server.go
+++ tfe/provider_server.go
@@ -2,6 +2,7 @@ package tfe
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
@@ -13,9 +14,10 @@ type providerServer struct {
 	providerMetaSchema *tfprotov5.Schema
 	resourceSchemas    map[string]*tfprotov5.Schema
 	dataSourceSchemas  map[string]*tfprotov5.Schema
+	meta               providerMeta
 
 	resourceRouter
-	dataSourceRouter
+	dataSourceRouter map[string]func(p *providerServer) tfprotov5.DataSourceServer
 }
 
 type providerMeta struct {
@@ -23,46 +25,66 @@ type providerMeta struct {
 	hostname string
 }
 
-func (s *providerServer) GetProviderSchema(ctx context.Context, req *tfprotov5.GetProviderSchemaRequest) (*tfprotov5.GetProviderSchemaResponse, error) {
+func (p *providerServer) GetProviderSchema(ctx context.Context, req *tfprotov5.GetProviderSchemaRequest) (*tfprotov5.GetProviderSchemaResponse, error) {
 	return &tfprotov5.GetProviderSchemaResponse{
-		Provider:          s.providerSchema,
-		ProviderMeta:      s.providerMetaSchema,
-		ResourceSchemas:   s.resourceSchemas,
-		DataSourceSchemas: s.dataSourceSchemas,
+		Provider:          p.providerSchema,
+		ProviderMeta:      p.providerMetaSchema,
+		ResourceSchemas:   p.resourceSchemas,
+		DataSourceSchemas: p.dataSourceSchemas,
 	}, nil
 }
 
-func (s *providerServer) PrepareProviderConfig(ctx context.Context, req *tfprotov5.PrepareProviderConfigRequest) (*tfprotov5.PrepareProviderConfigResponse, error) {
+func (p *providerServer) PrepareProviderConfig(ctx context.Context, req *tfprotov5.PrepareProviderConfigRequest) (*tfprotov5.PrepareProviderConfigResponse, error) {
 	return nil, nil
 }
 
-func (s *providerServer) ConfigureProvider(ctx context.Context, req *tfprotov5.ConfigureProviderRequest) (*tfprotov5.ConfigureProviderResponse, error) {
+func (p *providerServer) ConfigureProvider(ctx context.Context, req *tfprotov5.ConfigureProviderRequest) (*tfprotov5.ConfigureProviderResponse, error) {
+	resp := &tfprotov5.ConfigureProviderResponse{
+		Diagnostics: []*tfprotov5.Diagnostic{},
+	}
+	meta, err := retrieveProviderMeta(req)
+	if err != nil {
+		resp.Diagnostics = append(resp.Diagnostics, &tfprotov5.Diagnostic{
+			Severity: tfprotov5.DiagnosticSeverityError,
+			Summary:  "Error retrieving provider meta values from provider request",
+			Detail:   fmt.Sprintf("Error retrieving provider meta values from provider request", err.Error()),
+		})
+		return resp, nil
+	}
+	p.meta = meta
 
-	config := req.Config
-	val, _ := config.Unmarshal(tftypes.Object{
-		AttributeTypes: map[string]tftypes.Type{
-			"hostname":        tftypes.String,
-			"token":           tftypes.String,
-			"ssl_skip_verify": tftypes.Bool,
-		}})
-
-	var valMap map[string]tftypes.Value
-	_ = val.As(&valMap)
-
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@CONFIGURE PROVIDEDR  %v", valMap)
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@CONFIGURE PROVIDEDR  %v", valMap)
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@CONFIGURE PROVIDEDR  %v", valMap)
-
-	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@DATA SOURCCE ROUTER  %v", s.dataSourceRouter["tfe_remote_state"])
-
-	var diags []*tfprotov5.Diagnostic
-	return &tfprotov5.ConfigureProviderResponse{
-		Diagnostics: diags,
-	}, nil
+	return resp, nil
 }
 
-func (s *providerServer) StopProvider(ctx context.Context, req *tfprotov5.StopProviderRequest) (*tfprotov5.StopProviderResponse, error) {
+func (p *providerServer) StopProvider(ctx context.Context, req *tfprotov5.StopProviderRequest) (*tfprotov5.StopProviderResponse, error) {
 	return &tfprotov5.StopProviderResponse{}, nil
+}
+
+func (p *providerServer) ValidateDataSourceConfig(ctx context.Context, req *tfprotov5.ValidateDataSourceConfigRequest) (*tfprotov5.ValidateDataSourceConfigResponse, error) {
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR DATA SOURCE ROUTER VALIDATE")
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR DATA SOURCE ROUTER VALIDATE")
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR DATA SOURCE ROUTER VALIDATE")
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR DATA SOURCE ROUTER VALIDATE")
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR DATA SOURCE ROUTER VALIDATE")
+	ds, ok := p.dataSourceRouter[req.TypeName]
+	if !ok {
+		return nil, errUnsupportedDataSource(req.TypeName)
+	}
+	return ds(p).ValidateDataSourceConfig(ctx, req)
+}
+
+func (p *providerServer) ReadDataSource(ctx context.Context, req *tfprotov5.ReadDataSourceRequest) (*tfprotov5.ReadDataSourceResponse, error) {
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR DATA SOURCE ROUTER READ")
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR DATA SOURCE ROUTER READ")
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR DATA SOURCE ROUTER READ")
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR DATA SOURCE ROUTER READ")
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR DATA SOURCE ROUTER READ")
+	log.Printf("[DEBUG] @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OMAR DATA SOURCE ROUTER READ")
+	ds, ok := p.dataSourceRouter[req.TypeName]
+	if !ok {
+		return nil, errUnsupportedDataSource(req.TypeName)
+	}
+	return ds(p).ReadDataSource(ctx, req)
 }
 
 func ProviderServer() tfprotov5.ProviderServer {
@@ -92,7 +114,7 @@ func ProviderServer() tfprotov5.ProviderServer {
 			},
 		},
 		dataSourceSchemas: map[string]*tfprotov5.Schema{
-			"tfe_remote_state": {
+			"tfe_state_outputs": {
 				Version: 1,
 				Block: &tfprotov5.SchemaBlock{
 					Version: 1,
@@ -112,16 +134,53 @@ func ProviderServer() tfprotov5.ProviderServer {
 							Required:        true,
 						},
 						{
-							Name:     "state_output",
+							Name:     "values",
 							Type:     tftypes.DynamicPseudoType,
+							Optional: true,
 							Computed: true,
 						},
 					},
 				},
 			},
 		},
-		dataSourceRouter: dataSourceRouter{
-			"tfe_remote_state": dataSourceRemoteState{},
+		dataSourceRouter: map[string]func(p *providerServer) tfprotov5.DataSourceServer{
+			"tfe_state_outputs": newDataSourceRemoteState,
+
+			//	"tfe_remote_state": dataSourceRemoteState{},
 		},
 	}
+}
+
+func retrieveProviderMeta(req *tfprotov5.ConfigureProviderRequest) (providerMeta, error) {
+	meta := providerMeta{}
+	config := req.Config
+	val, err := config.Unmarshal(tftypes.Object{
+		AttributeTypes: map[string]tftypes.Type{
+			"hostname":        tftypes.String,
+			"token":           tftypes.String,
+			"ssl_skip_verify": tftypes.Bool,
+		}})
+
+	if err != nil {
+		return meta, fmt.Errorf("Could not unmarshal ConfigureProviderRequest %v", err)
+	}
+	var hostname string
+	var token string
+	var valMap map[string]tftypes.Value
+	err = val.As(&valMap)
+	if err != nil {
+		return meta, fmt.Errorf("Could not set the value to map %v", err)
+	}
+	err = valMap["hostname"].As(&hostname)
+	if err != nil {
+		return meta, fmt.Errorf("Could not set the hostname value to string %v", err)
+	}
+	err = valMap["token"].As(&token)
+	if err != nil {
+		return meta, fmt.Errorf("Could not set the token value to string %v", err)
+	}
+	meta.hostname = hostname
+	meta.token = token
+
+	return meta, nil
 }
