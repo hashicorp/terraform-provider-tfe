7f107f45ab5d01c99f984c06c8a5d47a54324a11
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-09-09 13:43:30 -0700

    Add loading/merging tests for cliConfig()

--- tfe/provider_test.go
+++ tfe/provider_test.go
@@ -189,6 +189,82 @@ func TestProvider_locateConfigFile(t *testing.T) {
 	}
 }
 
+func TestProvider_cliConfig(t *testing.T) {
+	// This only tests the behavior of merging various combinations of
+	// (credentials file, .terraformrc file, absent). Locating the .terraformrc
+	// file is tested separately.
+	originalHome := os.Getenv("HOME")
+	originalTfCliConfigFile := os.Getenv("TF_CLI_CONFIG_FILE")
+	reset := func() {
+		os.Setenv("HOME", originalHome)
+		if originalTfCliConfigFile != "" {
+			os.Setenv("TF_CLI_CONFIG_FILE", originalTfCliConfigFile)
+		} else {
+			os.Unsetenv("TF_CLI_CONFIG_FILE")
+		}
+	}
+	defer reset()
+
+	// Summary of fixtures: the credentials file and .terraformrc file each have
+	// credentials for two hosts, and they both have credentials for
+	// app.terraform.io
+	hasCredentials := "test-fixtures/env/home"
+	noCredentials := "test-fixtures/env/empty"
+	terraformrc := "test-fixtures/env/terraformrc"
+	noTerraformrc := "test-fixtures/env/nothing"
+	fromTerraformrc := "something.atlasv1.prod_rc_file"
+	fromCredentials := "something.atlasv1.prod_credentials_file"
+
+	cases := map[string]struct {
+		home            string
+		rcfile          string
+		expectCount     int
+		expectProdToken string
+	}{
+		"both main config and credentials JSON": {
+			home:            hasCredentials,
+			rcfile:          terraformrc,
+			expectCount:     3,
+			expectProdToken: fromTerraformrc,
+		},
+		"only main config": {
+			home:            noCredentials,
+			rcfile:          terraformrc,
+			expectCount:     2,
+			expectProdToken: fromTerraformrc,
+		},
+		"only credentials JSON": {
+			home:            hasCredentials,
+			rcfile:          noTerraformrc,
+			expectCount:     2,
+			expectProdToken: fromCredentials,
+		},
+		"neither file": {
+			home:            noCredentials,
+			rcfile:          noTerraformrc,
+			expectCount:     0,
+			expectProdToken: "",
+		},
+	}
+
+	for name, tc := range cases {
+		os.Setenv("HOME", tc.home)
+		os.Setenv("TF_CLI_CONFIG_FILE", tc.rcfile)
+		config := cliConfig()
+		credentialsCount := len(config.Credentials)
+		if credentialsCount != tc.expectCount {
+			t.Fatalf("%s: expected %d credentials, got %d", name, tc.expectCount, credentialsCount)
+		}
+		prodToken := ""
+		if config.Credentials["app.terraform.io"] != nil {
+			prodToken = config.Credentials["app.terraform.io"]["token"].(string)
+		}
+		if prodToken != tc.expectProdToken {
+			t.Fatalf("%s: expected %s as prod token, got %s", name, tc.expectProdToken, prodToken)
+		}
+	}
+}
+
 func testAccPreCheck(t *testing.T) {
 	// The credentials must be provided by the CLI config file for testing.
 	if diags := Provider().Configure(context.Background(), &terraform.ResourceConfig{}); diags.HasError() {
