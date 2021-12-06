e8cefe4842ad35c2fd1ceabe10805924b6a52012
Omar Ismail <oismail91@gmail.com>
2021-12-06 09:17:48 -0500

    OAuth Resource enhacements: key, secret, rsa_public_key

--- tfe/resource_tfe_oauth_client_test.go
+++ tfe/resource_tfe_oauth_client_test.go
@@ -42,6 +42,36 @@ func TestAccTFEOAuthClient_basic(t *testing.T) {
 	})
 }
 
+func TestAccTFEOAuthClient_rsaKeys(t *testing.T) {
+	oc := &tfe.OAuthClient{}
+	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckTFEOAuthClientDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccTFEOAuthClient_rsaKeys(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckTFEOAuthClientExists("tfe_oauth_client.foobar", oc),
+					testAccCheckTFEOAuthClientAttributes(oc),
+					resource.TestCheckResourceAttr(
+						"tfe_oauth_client.foobar", "api_url", "https://bbs.example.com"),
+					resource.TestCheckResourceAttr(
+						"tfe_oauth_client.foobar", "http_url", "https://bbs.example.com"),
+					resource.TestCheckResourceAttr(
+						"tfe_oauth_client.foobar", "service_provider", "bitbucket_server"),
+					resource.TestCheckResourceAttr(
+						"tfe_oauth_client.foobar", "key", "1e4843e138b0d44911a50d15e0f7cee4"),
+					resource.TestCheckResourceAttr(
+						"tfe_oauth_client.foobar", "rsa_public_key", "-----BEGIN PUBLIC KEY-----\nVGm9w0J8t6gWe745gW6E9NHJGiDKehh58bAtjO0wPvFg5l8Ea9s+PpAvP4wCZWDS\nhwIDAQAB\n-----END PUBLIC KEY-----\n"),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckTFEOAuthClientExists(
 	n string, oc *tfe.OAuthClient) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
@@ -74,16 +104,12 @@ func testAccCheckTFEOAuthClientExists(
 func testAccCheckTFEOAuthClientAttributes(
 	oc *tfe.OAuthClient) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		if oc.APIURL != "https://api.github.com" {
+		if oc.ServiceProvider == tfe.ServiceProviderGithub && oc.APIURL != "https://api.github.com" {
 			return fmt.Errorf("Bad API URL: %s", oc.APIURL)
 		}
 
-		if oc.HTTPURL != "https://github.com" {
+		if oc.ServiceProvider == tfe.ServiceProviderGithub && oc.HTTPURL != "https://github.com" {
 			return fmt.Errorf("Bad HTTP URL: %s", oc.HTTPURL)
-		}
-
-		if oc.ServiceProvider != tfe.ServiceProviderGithub {
-			return fmt.Errorf("Bad service provider: %s", oc.ServiceProvider)
 		}
 
 		return nil
@@ -125,4 +151,32 @@ resource "tfe_oauth_client" "foobar" {
   oauth_token      = "%s"
   service_provider = "github"
 }`, rInt, GITHUB_TOKEN)
+}
+
+func testAccTFEOAuthClient_rsaKeys(rInt int) string {
+	return fmt.Sprintf(`
+resource "tfe_organization" "foobar" {
+  name  = "tst-terraform-%d"
+  email = "admin@company.com"
+}
+
+resource "tfe_oauth_client" "foobar" {
+  organization     = tfe_organization.foobar.id
+	name 						 = "foobar_oauth"
+  api_url          = "https://bbs.example.com"
+  http_url         = "https://bbs.example.com"
+  service_provider = "bitbucket_server"
+  key       			 = "1e4843e138b0d44911a50d15e0f7cee4"
+  secret           = <<EOT
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAoKizy4xbN6qZFAwIJV24liz/vYBSvR3SjEiUzhpp0uMAmICN
+-----END RSA PRIVATE KEY-----
+EOT
+  rsa_public_key   = <<EOT
+-----BEGIN PUBLIC KEY-----
+VGm9w0J8t6gWe745gW6E9NHJGiDKehh58bAtjO0wPvFg5l8Ea9s+PpAvP4wCZWDS
+hwIDAQAB
+-----END PUBLIC KEY-----
+EOT
+}`, rInt)
 }
