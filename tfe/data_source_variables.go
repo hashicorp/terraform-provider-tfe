5b99284a4d76344feb5428b8dfd82c31f96eb10f
Rexredinger <redinger@hashicorp.com>
2022-03-02 16:17:32 -0500

    Add datasources for variable sets

--- tfe/data_source_variables.go
+++ tfe/data_source_variables.go
@@ -62,14 +62,26 @@ func dataSourceTFEWorkspaceVariables() *schema.Resource {
 				},
 			},
 			"workspace_id": {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:         schema.TypeString,
+				Required:     false,
+				ExactlyOneOf: []string{"workspace_id", "variable_set_id"},
+			},
+			"variable_set_id": {
+				Type:         schema.TypeString,
+				Required:     false,
+				ExactlyOneOf: []string{"workspace_id", "variable_set_id"},
 			},
 		},
 	}
 }
 
 func dataSourceVariableRead(d *schema.ResourceData, meta interface{}) error {
+	//Switch to variable set variable logic
+	_, variableSetIdProvided := d.GetOk("variable_set_id")
+	if variableSetIdProvided {
+		return dataSourceVariableSetVariableRead(d, meta)
+	}
+
 	tfeClient := meta.(*tfe.Client)
 
 	// Get the name and organization.
@@ -117,6 +129,60 @@ func dataSourceVariableRead(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	d.SetId(fmt.Sprintf("variables/%v", workspaceID))
+	d.Set("variables", append(totalTerraformVariables, totalEnvVariables...))
+	d.Set("terraform", totalTerraformVariables)
+	d.Set("env", totalEnvVariables)
+	return nil
+}
+
+func dataSourceVariableSetVariableRead(d *schema.ResourceData, meta interface{}) error {
+	tfeClient := meta.(*tfe.Client)
+
+	// Get the name and organization.
+	variableSetId := d.Get("variable_set_id").(string)
+
+	log.Printf("[DEBUG] Read configuration of variable set: %s", variableSetId)
+
+	totalEnvVariables := make([]interface{}, 0)
+	totalTerraformVariables := make([]interface{}, 0)
+
+	options := tfe.VariableSetVariableListOptions{}
+
+	for {
+		variableList, err := tfeClient.VariableSetVariables.List(ctx, variableSetId, options)
+		if err != nil {
+			return fmt.Errorf("Error retrieving variable list: %w", err)
+		}
+		terraformVars := make([]interface{}, 0)
+		envVars := make([]interface{}, 0)
+		for _, variable := range variableList.Items {
+			result := make(map[string]interface{})
+			result["id"] = variable.ID
+			result["category"] = variable.Category
+			result["hcl"] = variable.HCL
+			result["name"] = variable.Key
+			result["sensitive"] = variable.Sensitive
+			result["value"] = variable.Value
+			if variable.Category == "terraform" {
+				terraformVars = append(terraformVars, result)
+			} else if variable.Category == "env" {
+				envVars = append(envVars, result)
+			}
+		}
+
+		totalEnvVariables = append(totalEnvVariables, envVars...)
+		totalTerraformVariables = append(totalTerraformVariables, terraformVars...)
+
+		// Exit the loop when we've seen all pages.
+		if variableList.CurrentPage >= variableList.TotalPages {
+			break
+		}
+
+		// Update the page number to get the next page.
+		options.PageNumber = variableList.NextPage
+	}
+
+	d.SetId(fmt.Sprintf("variables/%v", variableSetId))
 	d.Set("variables", append(totalTerraformVariables, totalEnvVariables...))
 	d.Set("terraform", totalTerraformVariables)
 	d.Set("env", totalEnvVariables)
