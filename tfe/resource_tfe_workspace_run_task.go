fc7e7a60399d5958003a2ce48c66f784be58bc8f
Glenn Sarti <glennsarti@users.noreply.github.com>
2022-04-20 16:08:19 +0800

    Add Workspace Run Tasks Resource and Data source
    This commit adds the `data.tfe_workspace_run_task` and
    `resource.tfe_workspace_run_task` resources to the provider. These
    resources will be able to manage Workspace level Run Tasks.
    
    This commit also includes acceptance tests for these new resources.

---
+++ tfe/resource_tfe_workspace_run_task.go
@@ -0,0 +1,166 @@
+package tfe
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	tfe "github.com/hashicorp/go-tfe"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func resourceTFEWorkspaceRunTask() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceTFEWorkspaceRunTaskCreate,
+		Read:   resourceTFEWorkspaceRunTaskRead,
+		Delete: resourceTFEWorkspaceRunTaskDelete,
+		Update: resourceTFEWorkspaceRunTaskUpdate,
+		Importer: &schema.ResourceImporter{
+			State: resourceTFEWorkspaceRunTaskImporter,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"workspace_id": {
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Required: true,
+			},
+
+			"task_id": {
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Required: true,
+			},
+
+			"enforcement_level": {
+				Type:     schema.TypeString,
+				Required: true,
+				ValidateFunc: validation.StringInSlice(
+					[]string{
+						string(tfe.Advisory),
+						string(tfe.Mandatory),
+					},
+					false,
+				),
+			},
+		},
+	}
+}
+
+func resourceTFEWorkspaceRunTaskCreate(d *schema.ResourceData, meta interface{}) error {
+	tfeClient := meta.(*tfe.Client)
+
+	workspaceID := d.Get("workspace_id").(string)
+	taskID := d.Get("task_id").(string)
+
+	task, err := tfeClient.RunTasks.Read(ctx, taskID)
+	if err != nil {
+		return fmt.Errorf(
+			"Error retrieving task %s: %v", taskID, err)
+	}
+
+	ws, err := tfeClient.Workspaces.ReadByID(ctx, workspaceID)
+	if err != nil {
+		return fmt.Errorf(
+			"Error retrieving workspace %s: %v", workspaceID, err)
+	}
+
+	options := tfe.WorkspaceRunTaskCreateOptions{
+		RunTask:          task,
+		EnforcementLevel: tfe.TaskEnforcementLevel(d.Get("enforcement_level").(string)),
+	}
+
+	log.Printf("[DEBUG] Create task %s in workspace %s", task.ID, ws.ID)
+	wstask, err := tfeClient.WorkspaceRunTasks.Create(ctx, ws.ID, options)
+	if err != nil {
+		return fmt.Errorf("Error creating task %s in workspace %s: %v", task.ID, ws.ID, err)
+	}
+
+	d.SetId(wstask.ID)
+
+	return resourceTFEWorkspaceRunTaskRead(d, meta)
+}
+
+func resourceTFEWorkspaceRunTaskDelete(d *schema.ResourceData, meta interface{}) error {
+	tfeClient := meta.(*tfe.Client)
+
+	// Get the workspace
+	workspaceID := d.Get("workspace_id").(string)
+
+	log.Printf("[DEBUG] Delete task %s in workspace %s", d.Id(), workspaceID)
+	err := tfeClient.WorkspaceRunTasks.Delete(ctx, workspaceID, d.Id())
+	if err != nil && err != tfe.ErrResourceNotFound {
+		return fmt.Errorf("Error deleting task %s in workspace %s: %v", d.Id(), workspaceID, err)
+	}
+
+	return nil
+}
+
+func resourceTFEWorkspaceRunTaskUpdate(d *schema.ResourceData, meta interface{}) error {
+	tfeClient := meta.(*tfe.Client)
+
+	// Get the workspace
+	workspaceID := d.Get("workspace_id").(string)
+
+	// Setup the options struct
+	options := tfe.WorkspaceRunTaskUpdateOptions{}
+	if d.HasChange("enforcement_level") {
+		options.EnforcementLevel = tfe.TaskEnforcementLevel(d.Get("enforcement_level").(string))
+	}
+
+	log.Printf("[DEBUG] Update configuration of task %s in workspace %s", d.Id(), workspaceID)
+	_, err := tfeClient.WorkspaceRunTasks.Update(ctx, workspaceID, d.Id(), options)
+	if err != nil {
+		return fmt.Errorf("Error updating task %s in workspace %s: %v", d.Id(), workspaceID, err)
+	}
+
+	return nil
+}
+
+func resourceTFEWorkspaceRunTaskRead(d *schema.ResourceData, meta interface{}) error {
+	tfeClient := meta.(*tfe.Client)
+
+	// Get the workspace
+	workspaceID := d.Get("workspace_id").(string)
+
+	wstask, err := tfeClient.WorkspaceRunTasks.Read(ctx, workspaceID, d.Id())
+	if err != nil {
+		if err == tfe.ErrResourceNotFound {
+			log.Printf("[DEBUG] Workspace Task %s does not exist in workspace %s", d.Id(), workspaceID)
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("Error reading configuration of task %s in workspace %s: %v", d.Id(), workspaceID, err)
+	}
+
+	// Update the config.
+	d.Set("workspace_id", wstask.Workspace.ID)
+	d.Set("task_id", wstask.RunTask.ID)
+	d.Set("enforcement_level", string(wstask.EnforcementLevel))
+
+	return nil
+}
+
+func resourceTFEWorkspaceRunTaskImporter(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	tfeClient := meta.(*tfe.Client)
+
+	s := strings.Split(d.Id(), "/")
+	if len(s) != 3 {
+		return nil, fmt.Errorf(
+			"invalid task input format: %s (expected <ORGANIZATION>/<WORKSPACE NAME>/<TASK NAME>)",
+			d.Id(),
+		)
+	}
+
+	wstask, err := fetchWorkspaceRunTask(s[2], s[1], s[0], tfeClient)
+	if err != nil {
+		return nil, err
+	}
+
+	d.Set("workspace_id", wstask.Workspace.ID)
+	d.Set("task_id", wstask.RunTask.ID)
+	d.SetId(wstask.ID)
+
+	return []*schema.ResourceData{d}, nil
+}
---
+++ tfe/resource_tfe_workspace_run_task_test.go
@@ -0,0 +1,177 @@
+package tfe
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	tfe "github.com/hashicorp/go-tfe"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccTFEWorkspaceRunTask_create(t *testing.T) {
+	skipUnlessRunTasksDefined(t)
+	skipIfFreeOnly(t) // Run Tasks requires TFE or a TFC paid/trial subscription
+
+	workspaceTask := &tfe.WorkspaceRunTask{}
+	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
+	orgName := fmt.Sprintf("tst-terraform-%d", rInt)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckTFEWorkspaceRunTaskDestroy,
+		Steps: []resource.TestStep{
+			testCheckCreateOrgWithRunTasks(orgName),
+			{
+				Config: testAccTFEWorkspaceRunTask_basic(orgName, rInt, runTasksUrl()),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckTFEWorkspaceRunTaskExists("tfe_workspace_run_task.foobar", workspaceTask),
+					resource.TestCheckResourceAttr("tfe_workspace_run_task.foobar", "enforcement_level", "advisory"),
+				),
+			},
+			{
+				Config: testAccTFEWorkspaceRunTask_update(orgName, rInt, runTasksUrl()),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("tfe_workspace_run_task.foobar", "enforcement_level", "mandatory"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccTFEWorkspaceRunTask_import(t *testing.T) {
+	skipUnlessRunTasksDefined(t)
+	skipIfFreeOnly(t) // Run Tasks requires TFE or a TFC paid/trial subscription
+
+	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
+	orgName := fmt.Sprintf("tst-terraform-%d", rInt)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckTFETeamAccessDestroy,
+		Steps: []resource.TestStep{
+			testCheckCreateOrgWithRunTasks(orgName),
+			{
+				Config: testAccTFEWorkspaceRunTask_basic(orgName, rInt, runTasksUrl()),
+			},
+			{
+				ResourceName:      "tfe_workspace_run_task.foobar",
+				ImportState:       true,
+				ImportStateId:     fmt.Sprintf("tst-terraform-%d/workspace-test/foobar-task", rInt),
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckTFEWorkspaceRunTaskExists(n string, runTask *tfe.WorkspaceRunTask) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		tfeClient := testAccProvider.Meta().(*tfe.Client)
+
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No instance ID is set")
+		}
+
+		if rs.Primary.Attributes["workspace_id"] == "" {
+			return fmt.Errorf("No Workspace ID is set")
+		}
+
+		rt, err := tfeClient.WorkspaceRunTasks.Read(ctx, rs.Primary.Attributes["workspace_id"], rs.Primary.ID)
+		if err != nil {
+			return fmt.Errorf("error reading Workspace Run Task: %w", err)
+		}
+
+		if rt == nil {
+			return fmt.Errorf("Workspace Run Task not found")
+		}
+
+		*runTask = *rt
+
+		return nil
+	}
+}
+
+func testAccCheckTFEWorkspaceRunTaskDestroy(s *terraform.State) error {
+	tfeClient := testAccProvider.Meta().(*tfe.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "tfe_workspace_run_task" {
+			continue
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No instance ID is set")
+		}
+		if rs.Primary.Attributes["workspace_id"] == "" {
+			return fmt.Errorf("No Workspace ID is set")
+		}
+
+		_, err := tfeClient.WorkspaceRunTasks.Read(ctx, rs.Primary.Attributes["workspace_id"], rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("Workspace Run Tasks %s still exists", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccTFEWorkspaceRunTask_basic(orgName string, rInt int, runTaskUrl string) string {
+	return fmt.Sprintf(`
+resource "tfe_organization" "foobar" {
+  name  = "%s"
+  email = "admin@company.com"
+}
+
+resource "tfe_organization_run_task" "foobar" {
+  organization = tfe_organization.foobar.id
+  url          = "%s"
+  name         = "foobar-task"
+}
+
+resource "tfe_workspace" "foobar" {
+  name         = "workspace-test"
+  organization = tfe_organization.foobar.id
+}
+
+resource "tfe_workspace_run_task" "foobar" {
+  workspace_id      = resource.tfe_workspace.foobar.id
+  task_id           = resource.tfe_organization_run_task.foobar.id
+  enforcement_level = "advisory"
+}
+`, orgName, runTaskUrl)
+}
+
+func testAccTFEWorkspaceRunTask_update(orgName string, rInt int, runTaskUrl string) string {
+	return fmt.Sprintf(`
+resource "tfe_organization" "foobar" {
+  name  = "%s"
+  email = "admin@company.com"
+}
+
+resource "tfe_organization_run_task" "foobar" {
+  organization = tfe_organization.foobar.id
+  url          = "%s"
+  name         = "foobar-task"
+}
+
+resource "tfe_workspace" "foobar" {
+  name         = "workspace-test"
+  organization = tfe_organization.foobar.id
+}
+
+resource "tfe_workspace_run_task" "foobar" {
+  workspace_id      = resource.tfe_workspace.foobar.id
+  task_id           = resource.tfe_organization_run_task.foobar.id
+  enforcement_level = "mandatory"
+}
+`, orgName, runTaskUrl)
+}
