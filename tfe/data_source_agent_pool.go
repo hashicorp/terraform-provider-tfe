0e7f5815661f52f7ca6645ae5cc3598457f2b9bd
Jarrett Spiker <jarrett.spiker@hashicorp.com>
2022-07-20 17:29:09 -0400

    Allow agent pools to be imported by name

--- tfe/data_source_agent_pool.go
+++ tfe/data_source_agent_pool.go
@@ -1,8 +1,6 @@
 package tfe
 
 import (
-	"fmt"
-
 	tfe "github.com/hashicorp/go-tfe"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -32,34 +30,10 @@ func dataSourceTFEAgentPoolRead(d *schema.ResourceData, meta interface{}) error 
 	name := d.Get("name").(string)
 	organization := d.Get("organization").(string)
 
-	// Create an options struct.
-	// to reduce the number of pages returned, search based on the name. TFE instances which
-	// do not support agent pool search will just ignore the query parameter
-	options := tfe.AgentPoolListOptions{
-		Query: name,
+	id, err := fetchtAgentPoolId(organization, name, tfeClient)
+	if err != nil {
+		return err
 	}
-
-	for {
-		l, err := tfeClient.AgentPools.List(ctx, organization, &options)
-		if err != nil {
-			return fmt.Errorf("Error retrieving agent pools: %w", err)
-		}
-
-		for _, k := range l.Items {
-			if k.Name == name {
-				d.SetId(k.ID)
-				return nil
-			}
-		}
-
-		// Exit the loop when we've seen all pages.
-		if l.CurrentPage >= l.TotalPages {
-			break
-		}
-
-		// Update the page number to get the next page.
-		options.PageNumber = l.NextPage
-	}
-
-	return fmt.Errorf("Could not find agent pool %s/%s", organization, name)
+	d.SetId(id)
+	return nil
 }
