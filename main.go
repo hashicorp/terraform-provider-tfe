421a1201111455d23fd4acc53000dfcc5e528fb0
Michael Yocca <michael.yocca@hashicorp.com>
2022-08-12 15:55:46 -0700

    Enable debug mode on provider

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/hashicorp/go-plugin"
+	"github.com/hashicorp/terraform-exec/tfexec"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5/tf5server"
 	tfmux "github.com/hashicorp/terraform-plugin-mux"
@@ -24,6 +30,8 @@ func main() {
 	logFlags &^= (log.Ldate | log.Ltime)
 	log.SetFlags(logFlags)
 
+	debugFlag := flag.Bool("debug", false, "Start provider in debug mode.")
+	flag.Parse()
 	// terraform-plugin-mux here is used to combine multiple Terraform providers
 	// built using different SDK and frameworks in order to combine them into a
 	// single logical provider for Terraform to work with.
@@ -43,9 +51,58 @@ func main() {
 
 	err = tf5server.Serve(tfeProviderName, func() tfprotov5.ProviderServer {
 		return mux.Server()
-	})
+	}, withServeOptions(ctx, debugFlag)...)
 	if err != nil {
 		log.Printf("[ERROR] Could not start serving the ProviderServer: %v", err)
 		os.Exit(1)
 	}
+}
+
+func withServeOptions(ctx context.Context, debugFlag *bool) []tf5server.ServeOpt {
+	serveOpts := []tf5server.ServeOpt{}
+	if *debugFlag {
+		reattachConfigCh := make(chan *plugin.ReattachConfig)
+		go func() {
+			reattachConfig, err := waitForReattachConfig(reattachConfigCh)
+			if err != nil {
+				fmt.Printf("Error getting reattach config: %s\n", err)
+				return
+			}
+			printReattachConfig(reattachConfig)
+		}()
+		serveOpts = append(serveOpts, tf5server.WithDebug(ctx, reattachConfigCh, nil))
+	}
+	return serveOpts
+}
+
+func waitForReattachConfig(ch chan *plugin.ReattachConfig) (*plugin.ReattachConfig, error) {
+	select {
+	case config := <-ch:
+		return config, nil
+	case <-time.After(2 * time.Second):
+		return nil, fmt.Errorf("timeout waiting on reattach configuration")
+	}
+}
+
+func convertReattachConfig(reattachConfig *plugin.ReattachConfig) tfexec.ReattachConfig {
+	return tfexec.ReattachConfig{
+		Protocol: string(reattachConfig.Protocol),
+		Pid:      reattachConfig.Pid,
+		Test:     true,
+		Addr: tfexec.ReattachConfigAddr{
+			Network: reattachConfig.Addr.Network(),
+			String:  reattachConfig.Addr.String(),
+		},
+	}
+}
+
+func printReattachConfig(config *plugin.ReattachConfig) {
+	reattachStr, err := json.Marshal(map[string]tfexec.ReattachConfig{
+		tfeProviderName: convertReattachConfig(config),
+	})
+	if err != nil {
+		fmt.Printf("Error building reattach string: %s", err)
+		return
+	}
+	fmt.Printf("# Provider server started\nexport TF_REATTACH_PROVIDERS='%s'\n", string(reattachStr))
 }
