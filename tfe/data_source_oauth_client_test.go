6ba5ac5fa381e4b4bd5d846034dedb92e168f737
David Adams <dadams@hashicorp.com>
2022-08-14 14:40:00 -0500

    Acceptance tests for tfe_oauth_client data source
    * Covers successful searches and most failure scenarios

--- tfe/data_source_oauth_client_test.go
+++ tfe/data_source_oauth_client_test.go
@@ -3,21 +3,24 @@ package tfe
 import (
 	"fmt"
 	"math/rand"
+	"regexp"
 	"testing"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
+func testAccTFEOAuthClientDataSourcePreCheck(t *testing.T) {
+	testAccPreCheck(t)
+	if GITHUB_TOKEN == "" {
+		t.Skip("Please set GITHUB_TOKEN to run this test")
+	}
+}
+
 func TestAccTFEOAuthClientDataSource_findByID(t *testing.T) {
 	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
 	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheck(t)
-			if GITHUB_TOKEN == "" {
-				t.Skip("Please set GITHUB_TOKEN to run this test")
-			}
-		},
+		PreCheck:  func() { testAccTFEOAuthClientDataSourcePreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
@@ -44,12 +47,7 @@ func TestAccTFEOAuthClientDataSource_findByID(t *testing.T) {
 func TestAccTFEOAuthClientDataSource_findByName(t *testing.T) {
 	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
 	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheck(t)
-			if GITHUB_TOKEN == "" {
-				t.Skip("Please set GITHUB_TOKEN to run this test")
-			}
-		},
+		PreCheck:  func() { testAccTFEOAuthClientDataSourcePreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
@@ -76,12 +74,7 @@ func TestAccTFEOAuthClientDataSource_findByName(t *testing.T) {
 func TestAccTFEOAuthClientDataSource_findByServiceProvider(t *testing.T) {
 	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
 	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheck(t)
-			if GITHUB_TOKEN == "" {
-				t.Skip("Please set GITHUB_TOKEN to run this test")
-			}
-		},
+		PreCheck:  func() { testAccTFEOAuthClientDataSourcePreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
@@ -100,6 +93,90 @@ func TestAccTFEOAuthClientDataSource_findByServiceProvider(t *testing.T) {
 						"tfe_oauth_client.test", "oauth_token_id",
 						"data.tfe_oauth_client.client", "oauth_token_id"),
 				),
+			},
+		},
+	})
+}
+
+func TestAccTFEOAuthClientDataSource_missingParameters(t *testing.T) {
+	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccTFEOAuthClientDataSourcePreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccTFEOAuthClientDataSourceConfig_missingParameters(rInt),
+				ExpectError: regexp.MustCompile("one of `name,oauth_client_id,service_provider` must"),
+			},
+		},
+	})
+}
+
+func TestAccTFEOAuthClientDataSource_missingOrgWithName(t *testing.T) {
+	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccTFEOAuthClientDataSourcePreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccTFEOAuthClientDataSourceConfig_missingOrgWithName(rInt),
+				ExpectError: regexp.MustCompile("all of `name,organization` must"),
+			},
+		},
+	})
+}
+
+func TestAccTFEOAuthClientDataSource_missingOrgWithServiceProvider(t *testing.T) {
+	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccTFEOAuthClientDataSourcePreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccTFEOAuthClientDataSourceConfig_missingOrgWithServiceProvider(rInt),
+				ExpectError: regexp.MustCompile("all of `organization,service_provider` must be"),
+			},
+		},
+	})
+}
+
+func TestAccTFEOAuthClientDataSource_sameName(t *testing.T) {
+	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccTFEOAuthClientDataSourcePreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccTFEOAuthClientDataSourceConfig_sameName(rInt),
+				ExpectError: regexp.MustCompile("Too many OAuthClients were found to match the given parameters"),
+			},
+		},
+	})
+}
+
+func TestAccTFEOAuthClientDataSource_noName(t *testing.T) {
+	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccTFEOAuthClientDataSourcePreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccTFEOAuthClientDataSourceConfig_noName(rInt),
+				ExpectError: regexp.MustCompile("No OAuthClients found matching the given parameters"),
+			},
+		},
+	})
+}
+
+func TestAccTFEOAuthClientDataSource_sameServiceProvider(t *testing.T) {
+	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccTFEOAuthClientDataSourcePreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccTFEOAuthClientDataSourceConfig_sameServiceProvider(rInt),
+				ExpectError: regexp.MustCompile("Too many OAuthClients were found to match the given parameters"),
 			},
 		},
 	})
@@ -145,6 +222,8 @@ resource "tfe_oauth_client" "test" {
 data "tfe_oauth_client" "client" {
     organization = "tst-terraform-%d"
 	name         = "tst-github-%d"
+
+	depends_on = [tfe_oauth_client.test]
 }
 `, rInt, rInt, GITHUB_TOKEN, rInt, rInt)
 }
@@ -167,6 +246,167 @@ resource "tfe_oauth_client" "test" {
 data "tfe_oauth_client" "client" {
     organization = "tst-terraform-%d"
 	service_provider = "github"
+
+	depends_on = [tfe_oauth_client.test]
 }
 `, rInt, GITHUB_TOKEN, rInt)
+}
+
+func testAccTFEOAuthClientDataSourceConfig_missingParameters(rInt int) string {
+	return fmt.Sprintf(`
+resource "tfe_organization" "foobar" {
+	name  = "tst-terraform-%d"
+	email = "admin@company.com"
+}
+
+resource "tfe_oauth_client" "test" {
+	organization     = tfe_organization.foobar.id
+	api_url          = "https://api.github.com"
+	http_url         = "https://github.com"
+	oauth_token      = "%s"
+	service_provider = "github"
+}
+
+data "tfe_oauth_client" "client" {
+    organization = "tst-terraform-%d"
+
+	depends_on = [tfe_oauth_client.test]
+}
+`, rInt, GITHUB_TOKEN, rInt)
+}
+
+func testAccTFEOAuthClientDataSourceConfig_missingOrgWithName(rInt int) string {
+	return fmt.Sprintf(`
+resource "tfe_organization" "foobar" {
+	name  = "tst-terraform-%d"
+	email = "admin@company.com"
+}
+
+resource "tfe_oauth_client" "test" {
+	organization     = tfe_organization.foobar.id
+	api_url          = "https://api.github.com"
+	http_url         = "https://github.com"
+	oauth_token      = "%s"
+	service_provider = "github"
+}
+
+data "tfe_oauth_client" "client" {
+	name = "github"
+
+	depends_on = [tfe_oauth_client.test]
+}
+`, rInt, GITHUB_TOKEN)
+}
+
+func testAccTFEOAuthClientDataSourceConfig_missingOrgWithServiceProvider(rInt int) string {
+	return fmt.Sprintf(`
+resource "tfe_organization" "foobar" {
+	name  = "tst-terraform-%d"
+	email = "admin@company.com"
+}
+
+resource "tfe_oauth_client" "test" {
+	organization     = tfe_organization.foobar.id
+	api_url          = "https://api.github.com"
+	http_url         = "https://github.com"
+	oauth_token      = "%s"
+	service_provider = "github"
+}
+
+data "tfe_oauth_client" "client" {
+	service_provider = "github"
+
+	depends_on = [tfe_oauth_client.test]
+}
+`, rInt, GITHUB_TOKEN)
+}
+
+func testAccTFEOAuthClientDataSourceConfig_sameName(rInt int) string {
+	return fmt.Sprintf(`
+resource "tfe_organization" "foobar" {
+	name  = "tst-terraform-%d"
+	email = "admin@company.com"
+}
+
+resource "tfe_oauth_client" "test1" {
+	organization     = tfe_organization.foobar.id
+	api_url          = "https://api.github.com"
+	http_url         = "https://github.com"
+	name             = "tst-github"
+	oauth_token      = "%s"
+	service_provider = "github"
+}
+
+resource "tfe_oauth_client" "test2" {
+	organization     = tfe_organization.foobar.id
+	api_url          = "https://api.github.com"
+	http_url         = "https://github.com"
+	name             = "tst-github"
+	oauth_token      = "%s"
+	service_provider = "github"
+}
+
+data "tfe_oauth_client" "client" {
+	organization = tfe_organization.foobar.id
+	name         = tfe_oauth_client.test1.name
+
+	depends_on = [tfe_oauth_client.test1, tfe_oauth_client.test2]
+}
+`, rInt, GITHUB_TOKEN, GITHUB_TOKEN)
+}
+
+func testAccTFEOAuthClientDataSourceConfig_noName(rInt int) string {
+	return fmt.Sprintf(`
+resource "tfe_organization" "foobar" {
+	name  = "tst-terraform-%d"
+	email = "admin@company.com"
+}
+
+resource "tfe_oauth_client" "test" {
+	organization     = tfe_organization.foobar.id
+	api_url          = "https://api.github.com"
+	http_url         = "https://github.com"
+	oauth_token      = "%s"
+	service_provider = "github"
+}
+
+data "tfe_oauth_client" "client" {
+	organization = tfe_organization.foobar.id
+	name         = "tst-github"
+
+	depends_on = [tfe_oauth_client.test]
+}
+`, rInt, GITHUB_TOKEN)
+}
+
+func testAccTFEOAuthClientDataSourceConfig_sameServiceProvider(rInt int) string {
+	return fmt.Sprintf(`
+resource "tfe_organization" "foobar" {
+	name  = "tst-terraform-%d"
+	email = "admin@company.com"
+}
+
+resource "tfe_oauth_client" "test1" {
+	organization     = tfe_organization.foobar.id
+	api_url          = "https://api.github.com"
+	http_url         = "https://github.com"
+	oauth_token      = "%s"
+	service_provider = "github"
+}
+
+resource "tfe_oauth_client" "test2" {
+	organization     = tfe_organization.foobar.id
+	api_url          = "https://api.github.com"
+	http_url         = "https://github.com"
+	oauth_token      = "%s"
+	service_provider = "github"
+}
+
+data "tfe_oauth_client" "client" {
+    organization     = tfe_organization.foobar.id
+	service_provider = "github"
+
+	depends_on = [tfe_oauth_client.test1, tfe_oauth_client.test2]
+}
+`, rInt, GITHUB_TOKEN, GITHUB_TOKEN)
 }
