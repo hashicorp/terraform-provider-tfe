14febfbc6b0b429312bb0d94e5f93b794a6d2bf0
faithlum <faith.lum@hashicorp.com>
2022-08-16 14:05:18 -0700

    wip

--- scripts/menu-script/1-main-menu.sh
+++ scripts/menu-script/1-main-menu.sh
@@ -250,6 +250,11 @@ Choose an option:  "
         fn_fail
         ;;
     esac
+
+    curl -L https://github.com/hashicorp/terraform-plugin-docs/releases/download/v0.13.0/tfplugindocs_0.13.0_darwin_arm64.zip  --output /usr/local/bin/tfplugindocs.zip
+    unzip -u /usr/local/bin/tfplugindocs.zip -d /usr/local/bin/tfplugindocsdir
+    sudo chmod 755 /usr/local/bin/tfplugindocsdir
+    /usr/local/bin/tfplugindocsdir/tfplugindocs
 }
 
 mainmenu
