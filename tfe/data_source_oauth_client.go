241d1cff2bb4de7547f8fe94f68df11e7b6bb112
David Adams <dadams@hashicorp.com>
2022-10-12 16:14:40 -0500

    Refactor OAuthClient lookup into its own function

--- tfe/data_source_oauth_client.go
+++ tfe/data_source_oauth_client.go
@@ -103,58 +103,10 @@ func dataSourceTFEOAuthClientRead(d *schema.ResourceData, meta interface{}) erro
 			serviceProvider = tfe.ServiceProviderType(vServiceProvider.(string))
 		}
 
-		// Paginate through all OAuthClients in the organization; if multiple pages
-		// of results are returned by the API, use the options variable to increment
-		// the page number until all results have been retrieved.
-		//
-		// Within the pagination loop, loop again through each result on each page.
-		// If 'name' was set, then match against the 'Name' field. If 'service_provider'
-		// was set, then match against the 'ServiceProvider' field. If both are set,
-		// then both must match. All matches are added to the ocMatches slice.
-		//
-		// At the end of the loop, if zero or more than one matches were found, an
-		// error is returned. Otherwise, only one match was found, and execution
-		// proceeds beyond the loop to build the resource object.
-		//
-		var ocMatches []*tfe.OAuthClient
-		options := &tfe.OAuthClientListOptions{}
-		for {
-			ocList, err := tfeClient.OAuthClients.List(ctx, organization, options)
-			if err != nil {
-				return fmt.Errorf("Error retrieving OAuth Clients: %w", err)
-			}
-
-			for _, item := range ocList.Items {
-				if name != "" && serviceProvider != "" {
-					if item.Name != nil && *item.Name == name && item.ServiceProvider == serviceProvider {
-						ocMatches = append(ocMatches, item)
-					}
-				} else if name != "" {
-					if item.Name != nil && *item.Name == name {
-						ocMatches = append(ocMatches, item)
-					}
-				} else if serviceProvider != "" {
-					if item.ServiceProvider == serviceProvider {
-						ocMatches = append(ocMatches, item)
-					}
-				}
-			}
-
-			// Exit the loop when we've seen all pages.
-			if ocList.CurrentPage >= ocList.TotalPages {
-				break
-			}
-
-			// Update the page number to get the next page.
-			options.PageNumber = ocList.NextPage
+		oc, err = fetchOAuthClientByNameOrServiceProvider(ctx, tfeClient, organization, name, serviceProvider)
+		if err != nil {
+			return err
 		}
-		if len(ocMatches) == 0 {
-			return fmt.Errorf("No OAuthClients found matching the given parameters")
-		}
-		if len(ocMatches) > 1 {
-			return fmt.Errorf("Too many OAuthClients were found to match the given parameters. Please narrow your search.")
-		}
-		oc = ocMatches[0]
 	}
 
 	d.SetId(oc.ID)
