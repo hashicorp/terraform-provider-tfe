18c7f989534e63b6e7a56c86466f6d78559ed0e7
Nick Fagerlund <nick.fagerlund@gmail.com>
2023-04-12 14:35:06 -0700

    quickfast rename existing tfe_variable impl

--- tfe/provider.go
+++ tfe/provider.go
@@ -165,7 +165,7 @@ func Provider() *schema.Provider {
 			"tfe_terraform_version":           resourceTFETerraformVersion(),
 			"tfe_workspace":                   resourceTFEWorkspace(),
 			"tfe_workspace_run_task":          resourceTFEWorkspaceRunTask(),
-			"tfe_variable":                    resourceTFEVariable(),
+			"tfe_variable_old":                resourceTFEVariableOld(),
 			"tfe_variable_set":                resourceTFEVariableSet(),
 			"tfe_workspace_variable_set":      resourceTFEWorkspaceVariableSet(),
 			"tfe_workspace_policy_set":        resourceTFEWorkspacePolicySet(),
