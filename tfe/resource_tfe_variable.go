7fe16f9d19fa19c7224a8855be53cf1f61e4e157
Nick Fagerlund <nick.fagerlund@gmail.com>
2023-04-14 18:00:12 -0700

    Fill in the schema!

--- tfe/resource_tfe_variable.go
+++ tfe/resource_tfe_variable.go
@@ -3,8 +3,18 @@ package tfe
 import (
 	"context"
 
+	tfe "github.com/hashicorp/go-tfe"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 type resourceTFEVariable struct{}
@@ -17,8 +27,103 @@ func (*resourceTFEVariable) Metadata(_ context.Context, _ resource.MetadataReque
 // Schema implements resource.Resource
 func (*resourceTFEVariable) Schema(ctx context.Context, req resource.SchemaRequest, res *resource.SchemaResponse) {
 	res.Schema = schema.Schema{
-		Attributes:          map[string]schema.Attribute{},
-		Blocks:              map[string]schema.Block{},
+		Attributes: map[string]schema.Attribute{
+			"key": schema.StringAttribute{
+				Required:    true,
+				Description: "Name of the variable.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplaceIf(
+						func(ctx context.Context, req planmodifier.StringRequest, res *stringplanmodifier.RequiresReplaceIfFuncResponse) {
+							var stateSensitive types.Bool
+							diags := req.State.GetAttribute(ctx, path.Root("sensitive"), &stateSensitive)
+							if diags.HasError() {
+								res.Diagnostics.Append(diags...)
+								return
+							}
+							if stateSensitive.ValueBool() && req.PlanValue.ValueString() != req.StateValue.ValueString() {
+								res.RequiresReplace = true
+							}
+						},
+						"Force replacement if key changed and sensitive is true",
+						"Force replacement if key changed and sensitive is true",
+					),
+				},
+			},
+			"value": schema.StringAttribute{
+				Optional:    true,
+				Default:     stringdefault.StaticString(""),
+				Sensitive:   true,
+				Description: "Value of the variable",
+				// TODO: do descriptions cause a schema upgrade? how bout the rest of the stuff I'm doing here?
+			},
+			"category": schema.StringAttribute{
+				Required:    true,
+				Description: `Whether this is a Terraform or environment variable. Valid values are "terraform" or "env".`,
+				Validators: []validator.String{
+					stringvalidator.OneOf(
+						string(tfe.CategoryEnv),
+						string(tfe.CategoryTerraform),
+					),
+				},
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"description": schema.StringAttribute{
+				Optional: true,
+				Default:  stringdefault.StaticString(""),
+			},
+			"hcl": schema.BoolAttribute{
+				Optional: true,
+				Default:  booldefault.StaticBool(false),
+			},
+			"sensitive": schema.BoolAttribute{
+				Optional: true,
+				Default:  booldefault.StaticBool(false),
+				PlanModifiers: []planmodifier.Bool{
+					boolplanmodifier.RequiresReplaceIf(
+						func(ctx context.Context, req planmodifier.BoolRequest, res *boolplanmodifier.RequiresReplaceIfFuncResponse) {
+							if req.StateValue.ValueBool() && !req.ConfigValue.ValueBool() {
+								res.RequiresReplace = true
+							}
+						},
+						"Force replacement if sensitive argument changed from true to false.",
+						"Force replacement if sensitive argument changed from true to false.",
+					),
+				},
+			},
+			"workspace_id": schema.StringAttribute{
+				Optional: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+				Validators: []validator.String{
+					stringvalidator.ExactlyOneOf(
+						path.MatchRelative().AtParent().AtName("variable_set_id"),
+						// TODO: double-check behavior and ensure it includes current attr in that list
+					),
+					stringvalidator.RegexMatches(
+						workspaceIDRegexp,
+						"must be a valid workspace ID (ws-<RANDOM STRING>)",
+					),
+				},
+			},
+			"variable_set_id": schema.StringAttribute{
+				Optional: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+				Validators: []validator.String{
+					stringvalidator.ExactlyOneOf(
+						path.MatchRelative().AtParent().AtName("workspace_id"),
+					),
+					stringvalidator.RegexMatches(
+						variableSetIDRegexp,
+						"must be a valid variable set ID (varset-<RANDOM STRING>)",
+					),
+				},
+			},
+		},
 		Description:         "",
 		MarkdownDescription: "",
 		DeprecationMessage:  "",
