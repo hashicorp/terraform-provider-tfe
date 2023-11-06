c0eb98ae4d03747d4d98fda7a37f42c2c5c62391
Lauren <lauren.burwell@hashicorp.com>
2023-11-06 15:56:06 -0500

    remove fail fast bc it creates an unnecessary blocker for users

--- internal/provider/resource_tfe_agent_pool_allowed_workspaces.go
+++ internal/provider/resource_tfe_agent_pool_allowed_workspaces.go
@@ -43,42 +43,25 @@ func resourceTFEAgentPoolAllowedWorkspacesCreate(d *schema.ResourceData, meta in
 
 	apID := d.Get("agent_pool_id").(string)
 
-	// Before executing, make an API call to check the organization_scoped bool
-	// If organization_scoped is true, fail fast
-	// The purpose of this resource is to allow workspaces access to the agent pool
-	// When no workspaces have been given access
-	agentPool, err := config.Client.AgentPools.Read(ctx, apID)
-	if err != nil {
-		if errors.Is(err, tfe.ErrResourceNotFound) {
-			log.Printf("[DEBUG] agent pool %s no longer exists: ", apID)
-			return nil
-		}
-		return fmt.Errorf("Error reading configuration of agent pool: %s %w", apID, err)
-	}
-
 	// Create a new options struct.
 	options := tfe.AgentPoolAllowedWorkspacesUpdateOptions{}
 
-	if agentPool.OrganizationScoped {
-		return fmt.Errorf("error updating allowed workspaces on agent pool, workspaces already scoped for access to organization: %s", agentPool.Organization.Name)
-	} else if !agentPool.OrganizationScoped {
-		if allowedWorkspaceIDs, allowedWorkspaceSet := d.GetOk("allowed_workspace_ids"); allowedWorkspaceSet {
-			options.AllowedWorkspaces = []*tfe.Workspace{}
-			for _, workspaceID := range allowedWorkspaceIDs.(*schema.Set).List() {
-				if val, ok := workspaceID.(string); ok {
-					options.AllowedWorkspaces = append(options.AllowedWorkspaces, &tfe.Workspace{ID: val})
-				}
+	if allowedWorkspaceIDs, allowedWorkspaceSet := d.GetOk("allowed_workspace_ids"); allowedWorkspaceSet {
+		options.AllowedWorkspaces = []*tfe.Workspace{}
+		for _, workspaceID := range allowedWorkspaceIDs.(*schema.Set).List() {
+			if val, ok := workspaceID.(string); ok {
+				options.AllowedWorkspaces = append(options.AllowedWorkspaces, &tfe.Workspace{ID: val})
 			}
 		}
-
-		log.Printf("[DEBUG] Update agent pool: %s", apID)
-		_, err := config.Client.AgentPools.UpdateAllowedWorkspaces(ctx, apID, options)
-		if err != nil {
-			return fmt.Errorf("error updating agent pool %s: %w", apID, err)
-		}
-
-		d.SetId(apID)
 	}
+
+	log.Printf("[DEBUG] Update agent pool: %s", apID)
+	_, err := config.Client.AgentPools.UpdateAllowedWorkspaces(ctx, apID, options)
+	if err != nil {
+		return fmt.Errorf("Error updating agent pool %s: %w", apID, err)
+	}
+
+	d.SetId(apID)
 
 	return resourceTFEAgentPoolAllowedWorkspacesRead(d, meta)
 }
