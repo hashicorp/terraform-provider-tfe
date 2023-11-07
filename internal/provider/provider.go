a08afcc7d58a00359cd035c7ffcd1284dec5a6bb
Lauren <lauren.burwell@hashicorp.com>
2023-11-07 13:28:04 -0500

    alphabetize ResourceMap in provider.go

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -139,12 +139,12 @@ func Provider() *schema.Provider {
 			"tfe_terraform_version":              resourceTFETerraformVersion(),
 			"tfe_variable_set":                   resourceTFEVariableSet(),
 			"tfe_workspace":                      resourceTFEWorkspace(),
+			"tfe_workspace_agent_pool_execution": resourceTFEWorkspaceAgentPoolExecution(),
 			"tfe_workspace_policy_set":           resourceTFEWorkspacePolicySet(),
 			"tfe_workspace_policy_set_exclusion": resourceTFEWorkspacePolicySetExclusion(),
 			"tfe_workspace_run":                  resourceTFEWorkspaceRun(),
 			"tfe_workspace_run_task":             resourceTFEWorkspaceRunTask(),
 			"tfe_workspace_variable_set":         resourceTFEWorkspaceVariableSet(),
-			"tfe_workspace_agent_pool_execution": resourceTFEWorkspaceAgentPoolExecution(),
 		},
 		ConfigureContextFunc: configure(),
 	}
