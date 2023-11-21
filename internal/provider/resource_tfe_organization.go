4d4d4dfb267775a61348940bbfd7b7087d41760e
SwiftEngineer <harbinzerg@gmail.com>
2023-11-20 19:20:25 -0800

    remove extra space

--- internal/provider/resource_tfe_organization.go
+++ internal/provider/resource_tfe_organization.go
@@ -141,7 +141,6 @@ func resourceTFEOrganizationRead(d *schema.ResourceData, meta interface{}) error
 	d.Set("owners_team_saml_role_id", org.OwnersTeamSAMLRoleID)
 	d.Set("cost_estimation_enabled", org.CostEstimationEnabled)
 	d.Set("send_passing_statuses_for_untriggered_speculative_plans", org.SendPassingStatusesForUntriggeredSpeculativePlans)
-
 	// TFE (onprem) does not currently have this feature and this value won't be returned in those cases.
 	// org.AssessmentsEnforced will default to false
 	d.Set("assessments_enforced", org.AssessmentsEnforced)
