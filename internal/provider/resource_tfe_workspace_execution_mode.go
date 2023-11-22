35fa9891ec73e32c8d68c7edf3c0686e5b9c90c0
Lauren <lauren.burwell@hashicorp.com>
2023-11-21 19:44:44 -0500

    whent to set pool ID to nil

--- internal/provider/resource_tfe_workspace_execution_mode.go
+++ internal/provider/resource_tfe_workspace_execution_mode.go
@@ -74,7 +74,6 @@ func resourceTFEWorkspaceExecutionModeCreate(d *schema.ResourceData, meta interf
 	}
 
 	d.SetId(workspace.ID)
-	// d.Set() will update the resource state file for execution_mode and agent pool ID
 	d.Set("execution_mode", workspace.ExecutionMode)
 	d.Set("agent_pool_id", workspace.AgentPool.ID)
 
@@ -128,9 +127,11 @@ func resourceTFEWorkspaceExecutionModeUpdate(d *schema.ResourceData, meta interf
 	}
 
 	d.SetId(workspace.ID)
-	// d.Set() will update state file on tfe_workspace for execution_mode and agent pool ID
 	d.Set("execution_mode", workspace.ExecutionMode)
-	d.Set("agent_pool_id", workspace.AgentPool.ID)
+
+	if workspace.AgentPool == nil {
+		d.Set("agent_pool_id", nil)
+	}
 
 	return resourceTFEWorkspaceExecutionModeRead(d, meta)
 }
