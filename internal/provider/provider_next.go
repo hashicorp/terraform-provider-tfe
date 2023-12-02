354c0f38b64ec1a48abbea4d5e24147eb356e253
Teemu Matilainen <teemu.matilainen@iki.fi>
2023-12-02 18:06:24 +0200

    Add `tfe_registry_gpg_key` data source
    Retrieves a private registry GPG key.

--- internal/provider/provider_next.go
+++ internal/provider/provider_next.go
@@ -113,6 +113,7 @@ func (p *frameworkProvider) Configure(ctx context.Context, req provider.Configur
 
 func (p *frameworkProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
+		NewRegistryGPGKeyDataSource,
 		NewSAMLSettingsDataSource,
 	}
 }
