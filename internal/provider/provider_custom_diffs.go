dfab5d733e8f73553610f6f0e069f3effb6a155a
Brandon Croft <brandon.croft@gmail.com>
2023-12-06 14:33:46 -0700

    registry_gpg_key: Add plan mod for provider org default changes

--- internal/provider/provider_custom_diffs.go
+++ internal/provider/provider_custom_diffs.go
@@ -3,6 +3,9 @@ package provider
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -13,11 +16,30 @@ func customizeDiffIfProviderDefaultOrganizationChanged(c context.Context, diff *
 	plannedOrg := diff.Get("organization").(string)
 
 	if configOrg.IsNull() && config.Organization != plannedOrg {
-		// There is no organization configured on the resource, yet it is different from
-		// the state organization. We must conclude that the provider default organization changed.
+		// There is no organization configured on the resource, yet the provider org is different from
+		// the planned organization. We must conclude that the provider default organization changed.
 		if err := diff.SetNew("organization", config.Organization); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func modifyPlanForDefaultOrganizationChange(ctx context.Context, providerDefaultOrg string, req resource.ModifyPlanRequest, resp *resource.ModifyPlanResponse) {
+	if req.State.Raw.IsNull() {
+		return
+	}
+
+	orgPath := path.Root("organization")
+
+	var configOrg, plannedOrg *string
+	resp.Diagnostics.Append(req.Config.GetAttribute(ctx, orgPath, &configOrg)...)
+	resp.Diagnostics.Append(req.Plan.GetAttribute(ctx, orgPath, &plannedOrg)...)
+
+	if configOrg == nil && plannedOrg != nil && providerDefaultOrg != *plannedOrg {
+		// There is no organization configured on the resource, yet the provider org is different from
+		// the planned organization value. We must conclude that the provider default organization changed.
+		resp.Plan.SetAttribute(ctx, orgPath, types.StringValue(providerDefaultOrg))
+		resp.RequiresReplace.Append(orgPath)
+	}
 }
