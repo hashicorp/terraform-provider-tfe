7fe64992d995fc8b54b5614236bdc170b9d05199
Abdurahman Abdelgany <abdu.abdelgany@hashicorp.com>
2024-03-12 15:18:15 -0400

    wip

--- internal/provider/resource_tfe_test_variable_test.go
+++ internal/provider/resource_tfe_test_variable_test.go
@@ -11,6 +11,7 @@ import (
 
 	tfe "github.com/hashicorp/go-tfe"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccTFEVariable_testingvariables(t *testing.T) {
@@ -25,25 +26,57 @@ func TestAccTFEVariable_testingvariables(t *testing.T) {
 			{
 				Config: testAccTFETestVariable_test_variable(rInt),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckTFEVariableExists(
-						"tfe_variable.foobar", variable),
+					testAccCheckTFETestVariableExists(
+						"tfe_test_variable.foobar", variable),
 					testAccCheckTFEVariableAttributes(variable),
 					resource.TestCheckResourceAttr(
-						"tfe_variable.foobar", "key", "key_test"),
+						"tfe_test_variable.foobar", "key", "key_test"),
 					resource.TestCheckResourceAttr(
-						"tfe_variable.foobar", "value", "value_test"),
+						"tfe_test_variable.foobar", "value", "value_test"),
 					resource.TestCheckResourceAttr(
-						"tfe_variable.foobar", "description", "some description"),
+						"tfe_test_variable.foobar", "description", "some description"),
 					resource.TestCheckResourceAttr(
-						"tfe_variable.foobar", "category", "env"),
+						"tfe_test_variable.foobar", "category", "env"),
 					resource.TestCheckResourceAttr(
-						"tfe_variable.foobar", "hcl", "false"),
+						"tfe_test_variable.foobar", "hcl", "false"),
 					resource.TestCheckResourceAttr(
-						"tfe_variable.foobar", "sensitive", "false"),
+						"tfe_test_variable.foobar", "sensitive", "false"),
 				),
 			},
 		},
 	})
+}
+
+func testAccCheckTFETestVariableExists(
+	n string, variable *tfe.Variable) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		config := testAccProvider.Meta().(ConfiguredClient)
+
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No instance ID is set")
+		}
+		moduleID := tfe.RegistryModuleID{
+			Organization: rs.Primary.Attributes["organization"],
+			Name:         rs.Primary.Attributes["module_name"],
+			Provider:     rs.Primary.Attributes["module_provider"],
+			Namespace:    rs.Primary.Attributes["module_namespace"],
+			RegistryName: "private",
+		}
+
+		v, err := config.Client.TestVariables.Read(ctx, moduleID, rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		*variable = *v
+
+		return nil
+	}
 }
 
 func testAccTFETestVariable_test_variable(rInt int) string {
