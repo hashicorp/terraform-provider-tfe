2ef561d1ebc2033f32305149146caaca8c9050fa
Netra Mali <netra.mali@hashicorp.com>
2024-04-09 15:47:41 -0400

    lint error fixed

--- internal/provider/resource_tfe_project_oauth_client_test.go
+++ internal/provider/resource_tfe_project_oauth_client_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func TestAccTFEProjectOAuthClient_basic(t *testing.T) {	
+func TestAccTFEProjectOAuthClient_basic(t *testing.T) {
 	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
 
 	tfeClient, err := getClientUsingEnv()
@@ -56,7 +56,7 @@ func TestAccTFEProjectOAuthClient_basic(t *testing.T) {
 	})
 }
 
-func TestAccTFEProjectOAuthClient_incorrectImportSyntax(t *testing.T) {	
+func TestAccTFEProjectOAuthClient_incorrectImportSyntax(t *testing.T) {
 	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
 
 	tfeClient, err := getClientUsingEnv()
