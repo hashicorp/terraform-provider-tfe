e10907570727f9bbf923f1eb6b69849cfc714c0f
Sebastian Rivera <sebastian.rivera@hashicorp.com>
2024-05-09 18:12:15 -0400

    Dont skip TFE tests on org upgrade

--- internal/provider/helper_test.go
+++ internal/provider/helper_test.go
@@ -52,6 +52,10 @@ func testTfeClient(t *testing.T, options testClientOptions) *tfe.Client {
 // Attempts to upgrade an organization to the business plan. Requires a user token with admin access.
 // DEPRECATED : Please use the newSubscriptionUpdater instead.
 func upgradeOrganizationSubscription(t *testing.T, _ *tfe.Client, organization *tfe.Organization) {
+	if enterpriseEnabled() {
+		return
+	}
+
 	newSubscriptionUpdater(organization).WithBusinessPlan().Update(t)
 }
 
@@ -61,7 +65,9 @@ func createBusinessOrganization(t *testing.T, client *tfe.Client) (*tfe.Organiza
 		Email: tfe.String(fmt.Sprintf("%s@tfe.local", randomString(t))),
 	})
 
-	newSubscriptionUpdater(org).WithBusinessPlan().Update(t)
+	if !enterpriseEnabled() {
+		newSubscriptionUpdater(org).WithBusinessPlan().Update(t)
+	}
 
 	return org, orgCleanup
 }
