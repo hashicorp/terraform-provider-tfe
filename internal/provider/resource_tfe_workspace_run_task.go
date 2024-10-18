69adb223935a9c044e76dc27bd0abb78713fca33
Yulei Liu <yulei.liu@gmail.com>
2024-10-18 18:30:04 +1100

    change run)_task not found from error to warning

--- internal/provider/resource_tfe_workspace_run_task.go
+++ internal/provider/resource_tfe_workspace_run_task.go
@@ -122,7 +122,7 @@ func (r *resourceWorkspaceRunTask) Read(ctx context.Context, req resource.ReadRe
 	tflog.Debug(ctx, "Reading workspace run task")
 	wstask, err := r.config.Client.WorkspaceRunTasks.Read(ctx, workspaceID, wstaskID)
 	if err != nil {
-		resp.Diagnostics.AddError("Error reading Workspace Run Task", "Could not read Workspace Run Task, unexpected error: "+err.Error())
+		resp.Diagnostics.AddWarning("Error reading Workspace Run Task", "Could not read Workspace Run Task, unexpected error: "+err.Error())
 		return
 	}
 
