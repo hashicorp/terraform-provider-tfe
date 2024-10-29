544a3c2dc5e526eedb47c7c6fd17ecafef5a97cc
Netra Mali <104793044+netramali@users.noreply.github.com>
2024-10-29 13:17:46 -0400

    Update data_source_projects_test.go

--- internal/provider/data_source_projects_test.go
+++ internal/provider/data_source_projects_test.go
@@ -5,6 +5,7 @@ package provider
 
 import (
 	"fmt"
+	"github.com/hashicorp/go-tfe"
 	"math/rand"
 	"testing"
 	"time"
@@ -22,14 +23,15 @@ func TestAccTFEProjectsDataSource_basic(t *testing.T) {
 	orgName := org.Name
 
 	prj1 := createProject(t, tfeClient, org.Name, tfe.ProjectCreateOptions{
-		Name: "project1", Description: "Project 1",
+		Name: randomString(t),
 	})
 	prj2 := createProject(t, tfeClient, org.Name, tfe.ProjectCreateOptions{
-		Name: "project2", Description: "Project 2",
+		Name: randomString(t),
 	})
 	prj3 := createProject(t, tfeClient, org.Name, tfe.ProjectCreateOptions{
-		Name: "project3", Description: "Project 3",
+		Name: randomString(t),
 	})
+	prjNames := []string{"Default Project", prj1.Name, prj2.Name, prj3.Name}
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
@@ -44,58 +46,74 @@ func TestAccTFEProjectsDataSource_basic(t *testing.T) {
 						"data.tfe_projects.all", "projects.#", "4"),
 					resource.TestCheckResourceAttrSet(
 						"data.tfe_projects.all", "projects.0.id"),
-					resource.TestCheckResourceAttr(
-						"data.tfe_projects.all", "projects.0.name", "Default Project"),
+					resource.TestCheckResourceAttrWith(
+						"data.tfe_projects.all", "projects.0.name", func(value string) error {
+							for _, name := range prjNames {
+								if name == value {
+									return nil
+								}
+							}
+							return fmt.Errorf("Excepted project name %s to be in the list %v but not found. ", value, prjNames)
+						}),
 					resource.TestCheckResourceAttr(
 						"data.tfe_projects.all", "projects.0.description", ""),
 					resource.TestCheckResourceAttr(
 						"data.tfe_projects.all", "projects.0.organization", orgName),
 					resource.TestCheckResourceAttrSet(
 						"data.tfe_projects.all", "projects.1.id"),
-					resource.TestCheckResourceAttr(
-						"data.tfe_projects.all", "projects.1.name", "project1"),
-					resource.TestCheckResourceAttr(
-						"data.tfe_projects.all", "projects.1.description", "Project 1"),
+					resource.TestCheckResourceAttrWith(
+						"data.tfe_projects.all", "projects.1.name", func(value string) error {
+							for _, name := range prjNames {
+								if name == value {
+									return nil
+								}
+							}
+							return fmt.Errorf("Excepted project name %s to be in the list %v but not found. ", value, prjNames)
+						}),
 					resource.TestCheckResourceAttr(
 						"data.tfe_projects.all", "projects.1.organization", orgName),
 					resource.TestCheckResourceAttrSet(
 						"data.tfe_projects.all", "projects.2.id"),
-					resource.TestCheckResourceAttr(
-						"data.tfe_projects.all", "projects.2.name", "project2"),
-					resource.TestCheckResourceAttr(
-						"data.tfe_projects.all", "projects.2.description", "Project 2"),
+					resource.TestCheckResourceAttrWith(
+						"data.tfe_projects.all", "projects.2.name", func(value string) error {
+							for _, name := range prjNames {
+								if name == value {
+									return nil
+								}
+							}
+							return fmt.Errorf("Excepted project name %s to be in the list %v but not found. ", value, prjNames)
+						}),
 					resource.TestCheckResourceAttr(
 						"data.tfe_projects.all", "projects.2.organization", orgName),
 					resource.TestCheckResourceAttrSet(
 						"data.tfe_projects.all", "projects.3.id"),
-					resource.TestCheckResourceAttr(
-						"data.tfe_projects.all", "projects.3.name", "project3"),
-					resource.TestCheckResourceAttr(
-						"data.tfe_projects.all", "projects.3.description", "Project 3"),
+					resource.TestCheckResourceAttrWith(
+						"data.tfe_projects.all", "projects.3.name", func(value string) error {
+							for _, name := range prjNames {
+								if name == value {
+									return nil
+								}
+							}
+							return fmt.Errorf("Excepted project name %s to be in the list %v but not found. ", value, prjNames)
+						}),
 					resource.TestCheckResourceAttr(
 						"data.tfe_projects.all", "projects.3.organization", orgName),
-				  ),
+				),
 			},
 		},
 	})
 }
 
-
 func TestAccTFEProjectsDataSource_basicNoProjects(t *testing.T) {
-	tfeClient, err := getClientUsingEnv()
-	if err != nil {
-		t.Fatal(err)
-	}
-	org, orgCleanup := createBusinessOrganization(t, tfeClient)
-	t.Cleanup(orgCleanup)
-	orgName := org.Name
+	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
+	orgName := fmt.Sprintf("tst-terraform-%d", rInt)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV5ProviderFactories: testAccMuxedProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccTFEProjectsDataSourceConfig(orgName),
+				Config: testAccTFEProjectsDataSourceConfig_noProjects(orgName),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						"data.tfe_projects.all", "organization", orgName),
@@ -111,8 +129,20 @@ func TestAccTFEProjectsDataSource_basicNoProjects(t *testing.T) {
 
 func testAccTFEProjectsDataSourceConfig(orgName string) string {
 	return fmt.Sprintf(`
+	data tfe_projects "all" {
+	  organization = "%s"
+	}
+	`, orgName)
+}
+
+func testAccTFEProjectsDataSourceConfig_noProjects(orgName string) string {
+	return fmt.Sprintf(`
+resource "tfe_organization" "organization" {
+  name  = "%s"
+  email = "admin@tfe.local"
+}
 data tfe_projects "all" {
-  organization = "%s"
+  organization = tfe_organization.organization.name
 }
 `, orgName)
 }
