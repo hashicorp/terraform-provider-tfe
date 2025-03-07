8ebe3f1a4a8bacce6bfa1918629e40e8da4270a5
Chris Trombley <chris.trombley@hashicorp.com>
2025-03-06 17:40:29 -0800

    feat: upgrade tfe_policy_set_parameter resource to the provider framework

--- internal/provider/provider_next.go
+++ internal/provider/provider_next.go
@@ -132,12 +132,13 @@ func (p *frameworkProvider) DataSources(ctx context.Context) []func() datasource
 func (p *frameworkProvider) Resources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
 		NewAuditTrailTokenResource,
+		NewDataRetentionPolicyResource,
 		NewOrganizationRunTaskGlobalSettingsResource,
 		NewOrganizationRunTaskResource,
+		NewPolicySetParameterResource,
 		NewRegistryGPGKeyResource,
 		NewRegistryProviderResource,
 		NewResourceVariable,
-		NewDataRetentionPolicyResource,
 		NewResourceWorkspaceSettings,
 		NewSAMLSettingsResource,
 		NewStackResource,
