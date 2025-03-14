1e1b6180769a93cd81a681a74b95d348a8bb3f53
uturunku1 <luces.huayhuaca@gmail.com>
2025-03-14 11:23:50 -0700

    update the schema version number for tfe_variable

--- internal/provider/resource_tfe_variable.go
+++ internal/provider/resource_tfe_variable.go
@@ -264,7 +264,7 @@ func (r *resourceTFEVariable) Schema(ctx context.Context, req resource.SchemaReq
 		Description:         "",
 		MarkdownDescription: "",
 		DeprecationMessage:  "",
-		Version:             1,
+		Version:             0,
 	}
 }
 
