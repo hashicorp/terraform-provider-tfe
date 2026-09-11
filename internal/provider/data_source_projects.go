76d1d3064cade1aa9d9366476f3cd89451aa762f
Chris Trombley <chris.trombley@hashicorp.com>
2026-09-11 14:23:33 -0700

    chore: add pagination test

--- internal/provider/data_source_projects.go
+++ internal/provider/data_source_projects.go
@@ -159,11 +159,10 @@ func (d *dataSourceTFEProjects) Read(ctx context.Context, req datasource.ReadReq
 	model.Organization = types.StringValue(organization)
 	model.Projects = []modelTFEProjectsProject{}
 
-	pageSize := int32(100)
 	pageNumber := int32(1)
 	for { // paginate
 		query := &organizationsapi.ItemProjectsRequestBuilderGetQueryParameters{
-			Pagesize:   &pageSize,
+			Pagesize:   &dataSourceProjectsPageSize,
 			Pagenumber: &pageNumber,
 		}
 
